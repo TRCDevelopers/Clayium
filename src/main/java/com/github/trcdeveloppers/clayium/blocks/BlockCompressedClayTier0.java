93b69218d927b10de8669a00f741b7c3cad1635d
soramame0256 <62879324+soramame0256@users.noreply.github.com>
2023-08-18 14:02:00 +0900

    Toolなしでも採取できるように。

--- src/main/java/com/github/trcdeveloppers/clayium/blocks/BlockCompressedClayTier0.java
+++ src/main/java/com/github/trcdeveloppers/clayium/blocks/BlockCompressedClayTier0.java
@@ -1,25 +1,32 @@
 package com.github.trcdeveloppers.clayium.blocks;
 
 import com.github.trcdeveloppers.clayium.annotation.Block;
-import com.github.trcdeveloppers.clayium.items.IClayEnergy;
-import com.github.trcdeveloppers.clayium.items.ITieredItem;
+import com.github.trcdeveloppers.clayium.interfaces.IClayEnergy;
+import com.github.trcdeveloppers.clayium.interfaces.ITiered;
+import com.github.trcdeveloppers.clayium.util.UtilLocale;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+
+import javax.annotation.Nullable;
+import java.util.List;
 
 import static com.github.trcdeveloppers.clayium.creativetab.ClayiumCreativeTab.CLAYIUM;
 
 @Block(registryName = "compressed_clay_tier0")
-public class BlockCompressedClayTier0 extends ClayiumBlocks.ClayiumBlock implements IClayEnergy, ITieredItem {
+public class BlockCompressedClayTier0 extends ClayiumBlocks.ClayiumBlock implements IClayEnergy, ITiered {
     public BlockCompressedClayTier0(Material material) {
         super(material);
         this.setCreativeTab(CLAYIUM);
         this.setLightLevel(0f);
-        this.setHarvestLevel("shovel", 0);
-        this.setHardness(1f);
+        this.setHarvestLevel("shovel",0);
+        this.setHardness(0.5f);
         this.setSoundType(SoundType.GROUND);
     }
     public BlockCompressedClayTier0(){
-        this(Material.ROCK);
+        this(Material.GROUND);
     }
 
     @Override
@@ -30,5 +37,12 @@ public class BlockCompressedClayTier0 extends ClayiumBlocks.ClayiumBlock impleme
     @Override
     public int getTier() {
         return 0;
+    }
+
+    @Override
+    public void addInformation(ItemStack stack, @Nullable World worldIn, List<String> tooltip, ITooltipFlag flagIn) {
+        super.addInformation(stack,worldIn,tooltip,flagIn);
+        tooltip.add(UtilLocale.ClayEnergyNumeral(this.getClayEnergy()) + "CE");
+
     }
 }
