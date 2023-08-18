822ab685298a342ec434fc256068b9152769fd70
soramame0256 <62879324+soramame0256@users.noreply.github.com>
2023-08-18 12:10:03 +0900

    CompressedClayTier0の作成

---
+++ src/main/java/com/github/trcdeveloppers/clayium/blocks/BlockCompressedClayTier0.java
@@ -0,0 +1,29 @@
+package com.github.trcdeveloppers.clayium.blocks;
+
+import com.github.trcdeveloppers.clayium.annotation.Block;
+import com.github.trcdeveloppers.clayium.items.IClayEnergy;
+import com.github.trcdeveloppers.clayium.items.ITieredItem;
+import net.minecraft.block.material.Material;
+
+import static com.github.trcdeveloppers.clayium.creativetab.ClayiumCreativeTab.CLAYIUM;
+
+@Block(registryName = "compressed_clay_tier0")
+public class BlockCompressedClayTier0 extends ClayiumBlocks.ClayiumBlock implements IClayEnergy, ITieredItem {
+    public BlockCompressedClayTier0(Material material){
+        super(material);
+        this.setCreativeTab(CLAYIUM);
+        this.setLightLevel(0f);
+        this.setHarvestLevel("shovel",0);
+        this.setHardness(1f);
+    }
+
+    @Override
+    public long getClayEnergy() {
+        return this.getTier()>=4 ? (long)Math.pow(10,this.getTier()+1) : 0L;
+    }
+
+    @Override
+    public int getTier() {
+        return 0;
+    }
+}
