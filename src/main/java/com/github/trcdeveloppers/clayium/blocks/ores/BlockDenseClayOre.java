c748c785d5a33773cb10a33706dd3e2bf1cacd51
soramame0256 <62879324+soramame0256@users.noreply.github.com>
2023-08-18 15:33:09 +0900

    dense_clay_oreの実装

---
+++ src/main/java/com/github/trcdeveloppers/clayium/blocks/ores/BlockDenseClayOre.java
@@ -0,0 +1,26 @@
+package com.github.trcdeveloppers.clayium.blocks.ores;
+
+
+import com.github.trcdeveloppers.clayium.annotation.Block;
+import com.github.trcdeveloppers.clayium.blocks.ClayiumBlocks;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
+
+import static com.github.trcdeveloppers.clayium.creativetab.ClayiumCreativeTab.CLAYIUM;
+
+@Block(registryName = "dense_clay_ore")
+public class BlockDenseClayOre extends ClayiumBlocks.ClayiumBlock {
+    public BlockDenseClayOre(Material material) {
+        super(material);
+        this.setCreativeTab(CLAYIUM);
+        this.setLightLevel(0f);
+        this.setHarvestLevel("pickaxe", 1);
+        this.setHardness(3f);
+        this.setResistance(5f);
+        this.setSoundType(SoundType.STONE);
+    }
+
+    public BlockDenseClayOre() {
+        this(Material.ROCK);
+    }
+}
