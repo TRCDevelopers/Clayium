1278c6933ea7643498bb42766d92f006e96fb5c1
soramame0256 <62879324+soramame0256@users.noreply.github.com>
2023-08-18 14:02:26 +0900

    superの呼び出しの追加(addInformation)

--- src/main/java/com/github/trcdeveloppers/clayium/items/ItemClayPickaxe.java
+++ src/main/java/com/github/trcdeveloppers/clayium/items/ItemClayPickaxe.java
@@ -34,6 +34,7 @@ public class ItemClayPickaxe extends ItemPickaxe {
 
     @Override
     public void addInformation(ItemStack stack, @Nullable World worldIn, List<String> tooltip, ITooltipFlag flagIn) {
+        super.addInformation(stack,worldIn,tooltip,flagIn);
         tooltip.add(I18n.format("item.clay_pickaxe.tooltip.line1"));
     }
 }
