c3a49be273a9924231cb5aa01356c172cbfc444f
soramame0256 <62879324+soramame0256@users.noreply.github.com>
2023-08-18 12:32:45 +0900

    CompressedClayTier0のサウンドタイプをSANDに、マテリアルをROCKに変更

--- src/main/java/com/github/trcdeveloppers/clayium/blocks/BlockCompressedClayTier0.java
+++ src/main/java/com/github/trcdeveloppers/clayium/blocks/BlockCompressedClayTier0.java
@@ -3,18 +3,23 @@ package com.github.trcdeveloppers.clayium.blocks;
 import com.github.trcdeveloppers.clayium.annotation.Block;
 import com.github.trcdeveloppers.clayium.items.IClayEnergy;
 import com.github.trcdeveloppers.clayium.items.ITieredItem;
+import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 
 import static com.github.trcdeveloppers.clayium.creativetab.ClayiumCreativeTab.CLAYIUM;
 
 @Block(registryName = "compressed_clay_tier0")
 public class BlockCompressedClayTier0 extends ClayiumBlocks.ClayiumBlock implements IClayEnergy, ITieredItem {
-    public BlockCompressedClayTier0(Material material){
+    public BlockCompressedClayTier0(Material material) {
         super(material);
         this.setCreativeTab(CLAYIUM);
         this.setLightLevel(0f);
-        this.setHarvestLevel("shovel",0);
+        this.setHarvestLevel("shovel", 0);
         this.setHardness(1f);
+        this.setSoundType(SoundType.SAND);
+    }
+    public BlockCompressedClayTier0(){
+        this(Material.ROCK);
     }
 
     @Override
