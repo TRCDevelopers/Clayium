954ee11071c59d6dff92e6f6d8fc0a3102a4bebf
soramame0256 <62879324+soramame0256@users.noreply.github.com>
2023-08-19 13:22:25 +0900

    warn解消

--- src/main/java/com/github/trcdeveloppers/clayium/items/ItemClayShovel.java
+++ src/main/java/com/github/trcdeveloppers/clayium/items/ItemClayShovel.java
@@ -46,6 +46,7 @@ public class ItemClayShovel extends ItemSpade {
     @ParametersAreNonnullByDefault
     public void addInformation(ItemStack stack, @Nullable World worldIn, List<String> tooltip, ITooltipFlag flagIn) {
         super.addInformation(stack, worldIn, tooltip, flagIn);
+        if(this.getRegistryName()==null) return;
         List<String> list = UtilLocale.localizeTooltip(this.getRegistryName().getPath() + ".tooltip");
         if (list != null) {
             tooltip.addAll(list);
