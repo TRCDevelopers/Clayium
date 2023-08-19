2b654485a9bc44a038af16be4edbef2f28ea35ac
soramame0256 <62879324+soramame0256@users.noreply.github.com>
2023-08-19 13:02:29 +0900

    registryNameによるgetDestroySpeedの取得に変更。

--- src/main/java/com/github/trcdeveloppers/clayium/items/ItemClayPickaxe.java
+++ src/main/java/com/github/trcdeveloppers/clayium/items/ItemClayPickaxe.java
@@ -10,12 +10,13 @@ import net.minecraft.item.ItemPickaxe;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.NonNullList;
 import net.minecraft.world.World;
-import net.minecraftforge.common.ForgeHooks;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
 import javax.annotation.Nullable;
+import javax.annotation.ParametersAreNonnullByDefault;
 import java.util.List;
+import java.util.Objects;
 
 import static com.github.trcdeveloppers.clayium.creativetab.ClayiumCreativeTab.CLAYIUM;
 
@@ -30,6 +31,7 @@ public class ItemClayPickaxe extends ItemPickaxe {
     }
     @Override
     @SideOnly(Side.CLIENT)
+    @ParametersAreNonnullByDefault
     public void getSubItems(CreativeTabs tab, NonNullList<ItemStack> items){
         if(this.isInCreativeTab(tab)){
             items.add(new ItemStack(this, 1, 0));
@@ -37,16 +39,19 @@ public class ItemClayPickaxe extends ItemPickaxe {
     }
 
     @Override
+    @ParametersAreNonnullByDefault
     public float getDestroySpeed(ItemStack stack, IBlockState state) {
-        if(state.getBlock() == ClayiumBlocks.getBlock("clay_ore")
-                || state.getBlock() == ClayiumBlocks.getBlock("dense_clay_ore")
-                || state.getBlock() == ClayiumBlocks.getBlock("large_dense_clay_ore")) {
-            return state.getBlock().getHarvestLevel(state) <= stack.getItem().getHarvestLevel(stack, state.getBlock().getHarvestTool(state), null, state) ? this.efficiencyOnClayOre : this.efficiencyOnClayOre*100f/30f;
+        if(state.getBlock().getRegistryName() != null) {
+            String regName = state.getBlock().getRegistryName().toString();
+            if (regName.startsWith("clayium") && regName.endsWith("clay_ore")) {
+                return state.getBlock().getHarvestLevel(state) <= stack.getItem().getHarvestLevel(stack, Objects.requireNonNull(state.getBlock().getHarvestTool(state)), null, state) ? this.efficiencyOnClayOre : this.efficiencyOnClayOre * 100f / 30f;
+            }
         }
         return super.getDestroySpeed(stack,state);
     }
 
     @Override
+    @ParametersAreNonnullByDefault
     public void addInformation(ItemStack stack, @Nullable World worldIn, List<String> tooltip, ITooltipFlag flagIn) {
         super.addInformation(stack,worldIn,tooltip,flagIn);
         tooltip.add(I18n.format("item.clay_pickaxe.tooltip.line1"));
