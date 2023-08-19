84e4b9bf58e5e57a57b49c68f02767fc59e3ff15
soramame0256 <62879324+soramame0256@users.noreply.github.com>
2023-08-19 13:19:31 +0900

    clayshovelの実装。 clayoreに対するドロップ処理に関しては未

---
+++ src/main/java/com/github/trcdeveloppers/clayium/items/ItemClayShovel.java
@@ -0,0 +1,54 @@
+package com.github.trcdeveloppers.clayium.items;
+
+import com.github.trcdeveloppers.clayium.annotation.Item;
+import com.github.trcdeveloppers.clayium.util.UtilLocale;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.item.ItemSpade;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+import javax.annotation.Nullable;
+import javax.annotation.ParametersAreNonnullByDefault;
+import java.util.List;
+
+import static com.github.trcdeveloppers.clayium.creativetab.ClayiumCreativeTab.CLAYIUM;
+
+@Item(registryName = "clay_shovel")
+public class ItemClayShovel extends ItemSpade {
+    protected float efficiencyOnClayBlocks = 32.0f;
+    private float efficiencyOnClayOre = 12.0f;
+    public ItemClayShovel(){
+        super(ToolMaterial.WOOD);
+        this.setMaxDamage(500);
+        this.setCreativeTab(CLAYIUM);
+    }
+
+    @ParametersAreNonnullByDefault
+    @Override
+    public float getDestroySpeed(ItemStack stack, IBlockState state) {
+        if(state.getBlock().getMaterial(state)== Material.CLAY) {
+            return this.efficiencyOnClayBlocks;
+        }
+        if(state.getBlock().getRegistryName() != null) {
+            String regName = state.getBlock().getRegistryName().toString();
+            if (regName.startsWith("clayium") && regName.endsWith("clay_ore")) {
+                return this.efficiencyOnClayOre;
+            }
+        }
+        return super.getDestroySpeed(stack, state);
+    }
+    @Override
+    @SideOnly(Side.CLIENT)
+    @ParametersAreNonnullByDefault
+    public void addInformation(ItemStack stack, @Nullable World worldIn, List<String> tooltip, ITooltipFlag flagIn) {
+        super.addInformation(stack, worldIn, tooltip, flagIn);
+        List<String> list = UtilLocale.localizeTooltip(this.getRegistryName().getPath() + ".tooltip");
+        if (list != null) {
+            tooltip.addAll(list);
+        }
+    }
+}
