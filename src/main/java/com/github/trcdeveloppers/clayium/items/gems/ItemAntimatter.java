15ea503bca53b744a93aaf1ad422cfde0e8cb42f
bqc0n <b4c0nchan@gmail.com>
2023-08-25 13:26:07 +0900

    make antimatter a colored item

--- src/main/java/com/github/trcdeveloppers/clayium/items/gems/ItemAntimatter.java
+++ src/main/java/com/github/trcdeveloppers/clayium/items/gems/ItemAntimatter.java
@@ -1,6 +1,8 @@
 package com.github.trcdeveloppers.clayium.items.gems;
 
+import com.github.trcdeveloppers.clayium.interfaces.IColored;
 import com.github.trcdeveloppers.clayium.items.ClayiumItems;
+import net.minecraft.client.renderer.color.IItemColor;
 import net.minecraft.item.Item;
 
 import java.util.ArrayList;
@@ -10,7 +12,7 @@ import static com.github.trcdeveloppers.clayium.creativetab.ClayiumCreativeTab.C
 
 @SuppressWarnings("unused")
 @com.github.trcdeveloppers.clayium.annotation.Item(registryName = "antimatter")
-public class ItemAntimatter extends Item implements ClayiumItems.ClayiumItem {
+public class ItemAntimatter extends Item implements ClayiumItems.ClayiumItem, IColored {
 
     public ItemAntimatter() {
         super();
@@ -19,8 +21,13 @@ public class ItemAntimatter extends Item implements ClayiumItems.ClayiumItem {
 
     @Override
     public List<String> getOreDictionaries() {
-        List<String> oreDicts = new ArrayList<>();
-        oreDicts.add("gemAntimatter");
-        return oreDicts;
+        return new ArrayList<String>() {{
+            add("gemAntimatter");
+        }};
+    }
+
+    @Override
+    public IItemColor getColor() {
+        return ((stack, tintIndex) -> tintIndex == 0 ? 0x0000EB : tintIndex == 1 ? 0 : 0xFFFFFF);
     }
 }
