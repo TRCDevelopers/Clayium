06fb9b18579c73db97ca7077b8543d880fc174ad
bqc0n <b4c0nchan@gmail.com>
2023-10-10 14:30:02 +0900

    tileentity registration uses proxy

--- src/main/java/com/github/trcdeveloppers/clayium/common/ClayiumCommonProxy.java
+++ src/main/java/com/github/trcdeveloppers/clayium/common/ClayiumCommonProxy.java
@@ -1,12 +1,17 @@
 package com.github.trcdeveloppers.clayium.common;
 
 import com.github.trcdeveloppers.clayium.common.blocks.ClayiumBlocks;
+import com.github.trcdeveloppers.clayium.common.blocks.machines.clayworktable.TileClayWorkTable;
 import com.github.trcdeveloppers.clayium.common.items.ClayiumItems;
 import net.minecraft.block.Block;
 import net.minecraft.item.Item;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.fml.relauncher.Side;
+
+import static com.github.trcdeveloppers.clayium.Clayium.MOD_ID;
 
 public class ClayiumCommonProxy {
     @SubscribeEvent
@@ -17,6 +22,10 @@ public class ClayiumCommonProxy {
     @SubscribeEvent
     public void registerBlocks(RegistryEvent.Register<Block> event) {
         ClayiumBlocks.registerBlocks(event, Side.SERVER);
+    }
+
+    public void registerTileEntities() {
+        GameRegistry.registerTileEntity(TileClayWorkTable.class, new ResourceLocation(MOD_ID, "TileClayWorkTable"));
     }
 
     public void registerItemColors() {}
