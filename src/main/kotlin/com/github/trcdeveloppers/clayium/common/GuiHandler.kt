6bf537285a4a5ee8bd9286de9db8216a7724e05f
bqc0n <b4c0nchan@gmail.com>
2023-10-24 07:40:52 +0900

    gui handler

---
+++ src/main/kotlin/com/github/trcdeveloppers/clayium/common/GuiHandler.kt
@@ -0,0 +1,31 @@
+package com.github.trcdeveloppers.clayium.common
+
+import com.github.trcdeveloppers.clayium.client.gui.GuiClayWorkTable
+import com.github.trcdeveloppers.clayium.common.blocks.machines.clayworktable.ClayWorktableContainer
+import com.github.trcdeveloppers.clayium.common.blocks.machines.clayworktable.TileClayWorkTable
+import net.minecraft.entity.player.EntityPlayer
+import net.minecraft.util.math.BlockPos
+import net.minecraft.world.World
+import net.minecraftforge.fml.common.network.IGuiHandler
+
+class GuiHandler : IGuiHandler {
+    companion object {
+        const val CLAY_WORK_TABLE = 1
+    }
+
+    override fun getServerGuiElement(id: Int, player: EntityPlayer, world: World, x: Int, y: Int, z: Int): Any? {
+        val te = world.getTileEntity(BlockPos(x, y, z)) ?: return null
+        when (id) {
+            CLAY_WORK_TABLE -> return ClayWorktableContainer(player.inventory, te as TileClayWorkTable)
+        }
+        return null
+    }
+
+    override fun getClientGuiElement(id: Int, player: EntityPlayer, world: World, x: Int, y: Int, z: Int): Any? {
+        val te = world.getTileEntity(BlockPos(x, y, z)) ?: return null
+        when (id) {
+            CLAY_WORK_TABLE -> return GuiClayWorkTable(player.inventory, te as TileClayWorkTable)
+        }
+        return null
+    }
+}
