30689f2824f3969037148cae69d6c14133a934f7
bqc0n <b4c0nchan@gmail.com>
2023-10-28 14:16:11 +0900

    MachineBase

--- src/main/kotlin/com/github/trcdeveloppers/clayium/common/interfaces/IConnectable.kt
+++ src/main/kotlin/com/github/trcdeveloppers/clayium/common/interfaces/IConnectable.kt
@@ -3,24 +3,24 @@ package com.github.trcdeveloppers.clayium.common.interfaces
 import net.minecraft.util.EnumFacing
 
 interface IConnectable {
-    fun getInput(facing: EnumFacing?): Int {
+    fun getInput(facing: EnumFacing): Int {
         return -1
     }
 
-    fun getOutput(facing: EnumFacing?): Int {
+    fun getOutput(facing: EnumFacing): Int {
         return -1
     }
 
-    fun rollInput(facing: EnumFacing?): Int {
+    fun rollInput(facing: EnumFacing): Int {
         return -1
     }
 
-    fun rollOutput(facing: EnumFacing?): Int {
+    fun rollOutput(facing: EnumFacing): Int {
         return -1
     }
 
-    fun setInput(facing: EnumFacing?, mode: Int)
-    fun setOutput(facing: EnumFacing?, mode: Int)
-    fun clearInput(facing: EnumFacing?)
-    fun clearOutput(facing: EnumFacing?)
+    fun setInput(facing: EnumFacing, mode: Int)
+    fun setOutput(facing: EnumFacing, mode: Int)
+    fun clearInput(facing: EnumFacing)
+    fun clearOutput(facing: EnumFacing)
 }
