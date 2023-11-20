0d664b63035041a87fa817c9080347054f221469
bqc0n <b4c0nchan@gmail.com>
2023-11-21 08:49:53 +0900

    remove unnecessary networking classes

--- src/main/kotlin/com/github/trcdeveloppers/clayium/common/network/ClayiumPacketHandler.kt
+++
@@ -1,9 +0,0 @@
-package com.github.trcdeveloppers.clayium.common.network
-
-import com.github.trcdeveloppers.clayium.Clayium
-import net.minecraftforge.fml.common.network.NetworkRegistry
-import net.minecraftforge.fml.common.network.simpleimpl.SimpleNetworkWrapper
-
-object ClayiumPacketHandler {
-    val handler: SimpleNetworkWrapper = NetworkRegistry.INSTANCE.newSimpleChannel(Clayium.MOD_ID)
-}
