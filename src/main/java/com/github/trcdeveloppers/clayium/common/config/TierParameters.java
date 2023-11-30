cb0e796cd8c5ee09dcd95eb064b96a0ce2b95785
bqc0n <b4c0nchan@gmail.com>
2023-11-30 17:40:08 +0900

    add config

---
+++ src/main/java/com/github/trcdeveloppers/clayium/common/config/TierParameters.java
@@ -0,0 +1,14 @@
+package com.github.trcdeveloppers.clayium.common.config;
+
+import com.github.trcdeveloppers.clayium.Clayium;
+import net.minecraftforge.common.config.Config;
+import net.minecraftforge.common.config.Config.LangKey;
+import net.minecraftforge.common.config.Config.RangeInt;
+import net.minecraftforge.common.config.Config.Comment;
+
+@LangKey("clayium.config.tier")
+@Config(modid = Clayium.MOD_ID, name = Clayium.MOD_ID + "/tier_parameters")
+public class TierParameters {
+    @RangeInt(min = 0)
+    public static int[] bufferTransferIntervals = { 8, 8, 8, 8, 8, 4, 2, 1, 1, 1, 1, 1, 1, 1 };
+}
