0d03a5fcec1100b078bccaea74a2b1358e6b80d8
bqc0n <b4c0nchan@gmail.com>
2023-12-02 17:40:16 +0900

    add inventoryX, Y to TileClayBuffer

--- src/main/java/com/github/trcdeveloppers/clayium/common/config/ConfigTierParameters.java
+++ src/main/java/com/github/trcdeveloppers/clayium/common/config/ConfigTierParameters.java
@@ -4,11 +4,10 @@ import com.github.trcdeveloppers.clayium.Clayium;
 import net.minecraftforge.common.config.Config;
 import net.minecraftforge.common.config.Config.LangKey;
 import net.minecraftforge.common.config.Config.RangeInt;
-import net.minecraftforge.common.config.Config.Comment;
 
 @LangKey("clayium.config.tier")
 @Config(modid = Clayium.MOD_ID, name = Clayium.MOD_ID + "/tier_parameters")
-public class TierParameters {
+public class ConfigTierParameters {
     @RangeInt(min = 0)
     public static int[] bufferTransferIntervals = { 8, 8, 8, 8, 8, 4, 2, 1, 1, 1, 1, 1, 1, 1 };
 }
--- src/main/kotlin/com/github/trcdeveloppers/clayium/common/blocks/machine/claybuffer/TileClayBuffer.kt
+++ src/main/kotlin/com/github/trcdeveloppers/clayium/common/blocks/machine/claybuffer/TileClayBuffer.kt
@@ -1,6 +1,6 @@
 package com.github.trcdeveloppers.clayium.common.blocks.machine.claybuffer
 
-import com.github.trcdeveloppers.clayium.common.util.TierConstants
+import com.github.trcdeveloppers.clayium.common.config.ConfigTierParameters
 import net.minecraft.block.state.IBlockState
 import net.minecraft.nbt.NBTTagCompound
 import net.minecraft.network.NetworkManager
@@ -12,6 +12,7 @@ import net.minecraft.util.math.BlockPos
 import net.minecraft.world.World
 import net.minecraftforge.common.capabilities.Capability
 import net.minecraftforge.items.CapabilityItemHandler
+import net.minecraftforge.items.IItemHandler
 import net.minecraftforge.items.ItemStackHandler
 import java.lang.IllegalArgumentException
 
@@ -19,54 +20,58 @@ class TileClayBuffer(
     val tier: Int,
 ) : TileEntity(), ITickable {
 
-    private val coolDown = TierConstants.getTransferCooldon(tier)
+    val inventoryY: Int = when (tier) {
+        in 4..7 -> tier - 3
+        8, -> 4
+        in 9..13 -> 6
+        else -> throw IllegalArgumentException("Invalid tier for buffer: $tier")
+    }
 
-    // This does not use TierConstants because this only appears in this class.
-    private val handler = ItemStackHandler(
-        when (tier) {
-            // todo: fixme
-            1 -> 4
-            2 -> 8
-            3 -> 16
-            in 4..13 -> 16
-            else -> throw IllegalArgumentException("Invalid tier: $tier")
-        }
-    )
+    val inventoryX: Int = when (tier) {
+        in 4..7 -> tier - 2
+        in 8..13 -> 9
+        else -> throw IllegalArgumentException("Invalid tier for buffer: $tier")
+    }
+
+    private val transferInterval = try {
+        ConfigTierParameters.bufferTransferIntervals[tier]
+    } catch (e: IndexOutOfBoundsException) {
+        throw IllegalArgumentException("Invalid tier: $tier")
+    }
+
+    private val handler = ItemStackHandler(inventoryX * inventoryY)
 
     private val importingFaces: MutableSet<EnumFacing> = HashSet()
-    private val outputs: MutableSet<EnumFacing> = HashSet()
+    private val exportingFaces: MutableSet<EnumFacing> = HashSet()
     private var ticked: Int = 0
 
     override fun update() {
         if (world.isRemote) return
-        if (ticked < coolDown) {
+        if (ticked < transferInterval) {
             ticked++
             return
         }
 
         for (side in importingFaces) {
             val adjustHandler = this.world.getTileEntity(this.pos.offset(side))?.getCapability(ITEM_HANDLER_CAPABILITY, side.opposite) ?: continue
-            for (slot in 0 until adjustHandler.slots) {
-                val importingStack = adjustHandler.getStackInSlot(slot)
-                if (importingStack.isEmpty) continue
-                val remainedStack = handler.insertItem(0, importingStack, false)
-                adjustHandler.extractItem(slot, importingStack.count - remainedStack.count, false)
-                break
-            }
+            this.transferStack(adjustHandler, handler, 64)
         }
 
-        for (side in outputs) {
+        for (side in exportingFaces) {
             val adjustHandler = this.world.getTileEntity(this.pos.offset(side))?.getCapability(ITEM_HANDLER_CAPABILITY, side.opposite) ?: continue
-            for (slot in 0 until handler.slots) {
-                val exportingStack = handler.getStackInSlot(slot)
-                if (exportingStack.isEmpty) continue
-                val remainedStack = adjustHandler.insertItem(slot, exportingStack, false)
-                handler.extractItem(slot, exportingStack.count - remainedStack.count, false)
-                break
-            }
+            this.transferStack(handler, adjustHandler, 64)
         }
-
         ticked = 0
+    }
+
+    private fun transferStack(from: IItemHandler, to: IItemHandler, amount: Int) {
+        for (slot in 0 until from.slots) {
+            val stack = from.getStackInSlot(slot)
+            if (stack.isEmpty) continue
+            val remainedStack = to.insertItem(slot, stack, false)
+            from.extractItem(slot, stack.count - remainedStack.count, false)
+            break
+        }
     }
 
     override fun writeToNBT(compound: NBTTagCompound): NBTTagCompound {
@@ -74,7 +79,7 @@ class TileClayBuffer(
         for (side in importingFaces) {
             compound.setBoolean("input_${side.name2}", true)
         }
-        for (side in outputs) {
+        for (side in exportingFaces) {
             compound.setBoolean("output_${side.name2}", true)
         }
         return compound
@@ -84,7 +89,7 @@ class TileClayBuffer(
         super.readFromNBT(compound)
         for (side in EnumFacing.entries) {
             if (compound.getBoolean("input_${side.name2}")) importingFaces.add(side)
-            if (compound.getBoolean("output_${side.name2}")) outputs.add(side)
+            if (compound.getBoolean("output_${side.name2}")) exportingFaces.add(side)
         }
     }
 
@@ -107,6 +112,7 @@ class TileClayBuffer(
         return capability === ITEM_HANDLER_CAPABILITY || super.hasCapability(capability, facing)
     }
 
+    @Suppress("UNCHECKED_CAST")
     override fun <T : Any?> getCapability(capability: Capability<T>, facing: EnumFacing?): T? {
         return if (capability === ITEM_HANDLER_CAPABILITY) ITEM_HANDLER_CAPABILITY as T else super.getCapability(capability, facing)
     }
@@ -119,7 +125,7 @@ class TileClayBuffer(
         return side in importingFaces
     }
     fun getOutput(side: EnumFacing): Boolean {
-        return side in outputs
+        return side in exportingFaces
     }
 
     fun toggleInput(side: EnumFacing) {
@@ -127,12 +133,12 @@ class TileClayBuffer(
         this.markDirty()
     }
     fun toggleOutput(side: EnumFacing) {
-        if (side in outputs) outputs.remove(side) else outputs.add(side)
+        if (side in exportingFaces) exportingFaces.remove(side) else exportingFaces.add(side)
         this.markDirty()
     }
 
     companion object {
         @JvmStatic
-        private val ITEM_HANDLER_CAPABILITY = CapabilityItemHandler.ITEM_HANDLER_CAPABILITY
+        private val ITEM_HANDLER_CAPABILITY: Capability<IItemHandler> = CapabilityItemHandler.ITEM_HANDLER_CAPABILITY
     }
 }
