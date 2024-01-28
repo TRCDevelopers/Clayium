96c576aa956f92af4b87a7af07aeceab2d27144f
bqc0n <b4c0nchan@gmail.com>
2024-01-28 11:21:04 +0900

    add mod item base

---
+++ src/main/kotlin/com/github/trcdevelopers/clayium/common/items/ItemClayium.kt
@@ -0,0 +1,16 @@
+package com.github.trcdevelopers.clayium.common.items
+
+import com.github.trcdevelopers.clayium.common.Clayium
+import net.minecraft.item.Item
+import net.minecraft.util.ResourceLocation
+
+/**
+ * @param name name without mod id
+ */
+class ItemClayium(name: String) : Item() {
+    init {
+        this.creativeTab = Clayium.creativeTab
+        this.registryName = ResourceLocation(Clayium.MOD_ID, name)
+        this.translationKey = "${Clayium.MOD_ID}.$name"
+    }
+}
