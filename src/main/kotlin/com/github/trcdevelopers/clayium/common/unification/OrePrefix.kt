09748b23db5d4010166089993ac77f8fa8e14eb2
bqc0n <b4c0nchan@gmail.com>
2024-02-09 14:41:36 +0900

    create OrePrefix

--- src/main/kotlin/com/github/trcdevelopers/clayium/common/unification/OrePrefix.kt
+++ src/main/kotlin/com/github/trcdevelopers/clayium/common/unification/OrePrefix.kt
@@ -1,4 +1,21 @@
 package com.github.trcdevelopers.clayium.common.unification
 
-enum class OrePrefix {
+import com.github.trcdevelopers.clayium.common.unification.material.Material
+import com.github.trcdevelopers.clayium.common.unification.material.MaterialProperty
+
+class OrePrefix(
+    val name: String,
+    val generateCondition: (Material) -> Boolean,
+) {
+
+    fun doGenerateItem(material: Material): Boolean {
+        return generateCondition(material)
+    }
+
+    companion object {
+        val ingot = OrePrefix("ingot") { it.hasProperty<MaterialProperty.Ingot>() }
+        val matter = OrePrefix("matter") { it.hasProperty<MaterialProperty.Matter>() }
+        val dust = OrePrefix("dust") { it.hasProperty<MaterialProperty.Dust>() }
+        val plate = OrePrefix("plate") { it.hasProperty<MaterialProperty.Plate>() }
+    }
 }
