4e4fea5d7225a19a60e98ecbd15ab1a85c60c332
bqc0n <b4c0nchan@gmail.com>
2024-02-22 16:12:18 +0900

    fix IEnergizedClay to use value class ClayEnergy instead of raw long value

--- src/main/kotlin/com/github/trcdevelopers/clayium/common/interfaces/IEnergizedClay.kt
+++ src/main/kotlin/com/github/trcdevelopers/clayium/common/interfaces/IEnergizedClay.kt
@@ -1,5 +1,7 @@
 package com.github.trcdevelopers.clayium.common.interfaces
 
+import com.github.trcdevelopers.clayium.common.ClayEnergy
+
 interface IEnergizedClay {
-    fun getClayEnergy(): Long
+    fun getClayEnergy(): ClayEnergy
 }
