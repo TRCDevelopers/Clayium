258916329acc5b03c7495da6525968573c42f481
bqc0n <b4c0nchan@gmail.com>
2024-02-25 20:58:03 +0900

    still wip

--- src/main/kotlin/com/github/trcdevelopers/clayium/common/blocks/machine/single_single/BlockSingleSingle.kt
+++ src/main/kotlin/com/github/trcdevelopers/clayium/common/blocks/machine/single_single/BlockSingleSingle.kt
@@ -1,7 +1,10 @@
 package com.github.trcdevelopers.clayium.common.blocks.machine.single_single
 
-import net.minecraft.block.BlockContainer
+import com.github.trcdevelopers.clayium.common.Clayium
+import net.minecraft.block.Block
+import net.minecraft.block.SoundType
 import net.minecraft.block.material.Material
+import net.minecraft.block.properties.PropertyBool
 import net.minecraft.block.state.IBlockState
 import net.minecraft.entity.EntityLiving
 import net.minecraft.tileentity.TileEntity
@@ -9,22 +12,37 @@ import net.minecraft.util.math.BlockPos
 import net.minecraft.world.IBlockAccess
 import net.minecraft.world.World
 
-class BlockSingleSingle : BlockContainer(Material.IRON) {
+class BlockSingleSingle(
+    private val tier: Int,
+    private val teFactory: (Int) -> TileEntity,
+) : Block(Material.IRON) {
 
     init {
+        creativeTab = Clayium.creativeTab
+        blockSoundType = SoundType.METAL
+        this.setHardness(5.0f)
+        this.setResistance(10.0f)
+        this.setHarvestLevel("pickaxe", 1)
     }
 
-    override fun createNewTileEntity(world: World, meta: Int): TileEntity? {
-        // tierさえあればよいだろう
-        // tile(tier)
-        TODO("Not yet implemented")
+    override fun hasTileEntity(state: IBlockState): Boolean {
+        return true
+    }
+
+    override fun createTileEntity(world: World, state: IBlockState): TileEntity? {
+        return teFactory(tier)
     }
 
     override fun canCreatureSpawn(state: IBlockState, world: IBlockAccess, pos: BlockPos, type: EntityLiving.SpawnPlacementType): Boolean {
         return false
     }
 
-    companion object {
+    override fun isFullBlock(state: IBlockState) = !state.getValue(IS_PIPE)
+    override fun isFullCube(state: IBlockState) = isFullBlock(state)
+    override fun isOpaqueCube(state: IBlockState) = isFullBlock(state)
+    override fun causesSuffocation(state: IBlockState) = isFullBlock(state)
 
+    companion object {
+        private val IS_PIPE = PropertyBool.create("is_pipe")
     }
 }
