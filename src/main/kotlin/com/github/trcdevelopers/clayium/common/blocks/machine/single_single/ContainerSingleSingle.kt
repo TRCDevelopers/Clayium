cc6665dedd35ccee4feb13fd7f6eb7229981a429
bqc0n <b4c0nchan@gmail.com>
2024-03-01 18:15:22 +0900

    fix transferStackInSlot

--- src/main/kotlin/com/github/trcdevelopers/clayium/common/blocks/machine/single_single/ContainerSingleSingle.kt
+++ src/main/kotlin/com/github/trcdevelopers/clayium/common/blocks/machine/single_single/ContainerSingleSingle.kt
@@ -1,7 +1,9 @@
 package com.github.trcdevelopers.clayium.common.blocks.machine.single_single
 
 import com.github.trcdevelopers.clayium.common.blocks.machine.ContainerClayium
+import net.minecraft.entity.player.EntityPlayer
 import net.minecraft.inventory.IInventory
+import net.minecraft.item.ItemStack
 import net.minecraftforge.items.CapabilityItemHandler
 import net.minecraftforge.items.SlotItemHandler
 
@@ -18,9 +20,56 @@ class ContainerSingleSingle(
             }
         })
         addSlotToContainer(object : SlotItemHandler(itemHandler, 1, 116, 35) {
+            override fun isItemValid(stack: ItemStack): Boolean {
+                return false
+            }
             override fun onSlotChanged() {
                 tile.markDirty()
             }
         })
+    }
+
+    override fun transferStackInSlot(playerIn: EntityPlayer, index: Int): ItemStack {
+        println("clicked: $index")
+        val slot = inventorySlots[index] ?: return ItemStack.EMPTY
+        if (!slot.hasStack) return ItemStack.EMPTY
+        val slotStack = slot.stack
+        val orgStack = slotStack.copy()
+
+        when (index) {
+            // player inventory -> container? -> hot bar
+            in 0..<27 -> {
+                if (!(mergeItemStack(slotStack, 36, inventorySlots.size, false)
+                            || mergeItemStack(slotStack, 0, 8, false))) {
+                    return ItemStack.EMPTY
+                }
+            }
+            // hot bar -> container? -> player inventory
+            in 27..<36 -> {
+                if (!(mergeItemStack(slotStack, 36, inventorySlots.size, false)
+                            || mergeItemStack(slotStack, 9, 35, false))) {
+                    return ItemStack.EMPTY
+                }
+            }
+            // container -> hot bar? -> player inventory
+            in 36..<inventorySlots.size -> {
+                if (!mergeItemStack(slotStack, 0, 35, false)) {
+                    return ItemStack.EMPTY
+                }
+            }
+        }
+
+        if (slotStack.isEmpty) {
+            slot.putStack(ItemStack.EMPTY)
+        } else {
+            slot.onSlotChanged()
+        }
+
+        // no changes were made
+        if (slotStack.count == orgStack.count) {
+            return ItemStack.EMPTY
+        }
+        slot.onTake(playerIn, slotStack)
+        return orgStack
     }
 }
