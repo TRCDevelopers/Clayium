71613658218c1c80ee6f2fd631345735bea928cd
bqc0n <b4c0nchan@gmail.com>
2024-03-08 12:46:41 +0900

    create MachineModels

---
+++ src/main/kotlin/com/github/trcdevelopers/clayium/client/model/MachineBakedModel.kt
@@ -0,0 +1,121 @@
+package com.github.trcdevelopers.clayium.client.model
+
+import com.github.trcdevelopers.clayium.common.blocks.machine.BlockMachine
+import com.github.trcdevelopers.clayium.common.blocks.machine.MachineIoMode
+import com.github.trcdevelopers.clayium.common.blocks.machine.MachineIoMode.ALL
+import com.github.trcdevelopers.clayium.common.blocks.machine.MachineIoMode.CE
+import com.github.trcdevelopers.clayium.common.blocks.machine.MachineIoMode.NONE
+import com.github.trcdevelopers.clayium.common.blocks.machine.MachineIoMode.SLOT_1
+import com.github.trcdevelopers.clayium.common.blocks.machine.MachineIoMode.SLOT_2
+import com.google.common.collect.ImmutableList
+import net.minecraft.block.state.IBlockState
+import net.minecraft.client.renderer.block.model.BakedQuad
+import net.minecraft.client.renderer.block.model.BlockFaceUV
+import net.minecraft.client.renderer.block.model.BlockPartFace
+import net.minecraft.client.renderer.block.model.FaceBakery
+import net.minecraft.client.renderer.block.model.IBakedModel
+import net.minecraft.client.renderer.block.model.ItemOverrideList
+import net.minecraft.client.renderer.block.model.ModelRotation
+import net.minecraft.client.renderer.texture.TextureAtlasSprite
+import net.minecraft.util.EnumFacing
+import net.minecraft.util.ResourceLocation
+import net.minecraftforge.common.property.IExtendedBlockState
+import org.lwjgl.util.vector.Vector3f
+import java.util.function.Function
+
+class MachineBakedModel(
+    bakedTextureGetter: Function<ResourceLocation, TextureAtlasSprite>,
+    machineHullLocation: ResourceLocation
+) : IBakedModel {
+
+    private val machineHull = bakedTextureGetter.apply(machineHullLocation)
+    private val inputTextures: List<TextureAtlasSprite?> = MachineIoMode.entries.map {
+        if (it == NONE) return@map null
+        bakedTextureGetter.apply(getInputLocation(it))
+    }
+    private val outputTextures: List<TextureAtlasSprite?> = MachineIoMode.entries.map {
+        if (it == NONE) return@map null
+        bakedTextureGetter.apply(getOutputLocation(it))
+    }
+
+    private val machineHullQuads = EnumFacing.VALUES.map {
+        createQuad(it, machineHull)
+    }
+
+
+    /**
+     * value is null if the mode has no texture (i.e. NONE)
+     */
+    private val inputQuads: Map<MachineIoMode, List<BakedQuad>?> = MachineIoMode.entries.associateWith { mode ->
+        EnumFacing.VALUES.map {
+            val atlas = inputTextures[mode.id] ?: return@associateWith null
+                createQuad(it, atlas)
+        }
+    }
+
+    /**
+     * value is null if the mode has no texture (i.e. NONE)
+     */
+    private val outputQuads: Map<MachineIoMode, List<BakedQuad>?> = MachineIoMode.entries.associateWith { mode ->
+        EnumFacing.VALUES.map {
+            val atlas = outputTextures[mode.id] ?: return@associateWith null
+                createQuad(it, atlas)
+        }
+    }
+
+    override fun getQuads(state: IBlockState?, side: EnumFacing?, rand: Long): List<BakedQuad> {
+        if (state == null || side == null) return emptyList()
+
+        val extState = state as IExtendedBlockState
+        val quads = mutableListOf(machineHullQuads[side.index])
+
+        val inputMode = extState.getValue(BlockMachine.INPUTS)[side.index]
+        inputQuads[inputMode]?.let { quads.add(it[side.index]) }
+
+        val outputMode = extState.getValue(BlockMachine.OUTPUTS)[side.index]
+        outputQuads[outputMode]?.let { quads.add(it[side.index]) }
+
+        return ImmutableList.copyOf(quads)
+    }
+
+    override fun isAmbientOcclusion() = true
+    override fun isGui3d() = true
+    override fun isBuiltInRenderer() = false
+    override fun getParticleTexture() = machineHull
+    override fun getOverrides(): ItemOverrideList = ItemOverrideList.NONE
+
+    companion object {
+        private val faceBakery = FaceBakery()
+
+        private fun createQuad(side: EnumFacing, texture: TextureAtlasSprite): BakedQuad {
+            return faceBakery.makeBakedQuad(
+                Vector3f(0f, 0f, 0f),
+                Vector3f(16f, 16f, 16f),
+                BlockPartFace(null, 0, "", BlockFaceUV(floatArrayOf(0f, 0f, 16f, 16f), 0)),
+                texture,
+                side, ModelRotation.X0_Y0,
+                null, true, true,
+            )
+        }
+
+        private fun getInputLocation(mode: MachineIoMode): ResourceLocation {
+            return when (mode) {
+                SLOT_1 -> ResourceLocation("clayium:blocks/import_1")
+                SLOT_2 -> ResourceLocation("clayium:blocks/import_2")
+                ALL -> ResourceLocation("clayium:blocks/import")
+                CE -> ResourceLocation("clayium:blocks/import_energy")
+                NONE -> throw IllegalArgumentException("No texture for mode NONE")
+            }
+        }
+
+        private fun getOutputLocation(mode: MachineIoMode): ResourceLocation {
+            return when (mode) {
+                SLOT_1 -> ResourceLocation("clayium:blocks/export_1")
+                SLOT_2 -> ResourceLocation("clayium:blocks/export_2")
+                ALL -> ResourceLocation("clayium:blocks/export")
+                CE -> throw IllegalArgumentException("Export CE is invalid")
+                NONE -> throw IllegalArgumentException("No texture for mode NONE")
+            }
+        }
+    }
+}
---
+++ src/main/kotlin/com/github/trcdevelopers/clayium/client/model/MachineModel.kt
@@ -0,0 +1,38 @@
+package com.github.trcdevelopers.clayium.client.model
+
+import com.google.common.collect.ImmutableList
+import net.minecraft.client.renderer.block.model.IBakedModel
+import net.minecraft.client.renderer.texture.TextureAtlasSprite
+import net.minecraft.client.renderer.vertex.VertexFormat
+import net.minecraft.util.ResourceLocation
+import net.minecraftforge.client.model.IModel
+import net.minecraftforge.common.model.IModelState
+import java.util.function.Function
+
+class MachineModel(
+    private val machineHullTexture: ResourceLocation
+) : IModel {
+
+    override fun getTextures(): Collection<ResourceLocation> {
+        return ImmutableList.of(
+            machineHullTexture,
+            ResourceLocation("clayium:blocks/import_energy"),
+            ResourceLocation("clayium:blocks/import"),
+            ResourceLocation("clayium:blocks/import_1"),
+            ResourceLocation("clayium:blocks/import_2"),
+            ResourceLocation("clayium:blocks/import_12"),
+            ResourceLocation("clayium:blocks/export"),
+            ResourceLocation("clayium:blocks/export_1"),
+            ResourceLocation("clayium:blocks/export_2"),
+            ResourceLocation("clayium:blocks/export_12"),
+        )
+    }
+
+    override fun bake(
+        state: IModelState,
+        format: VertexFormat,
+        bakedTextureGetter: Function<ResourceLocation, TextureAtlasSprite>
+    ): IBakedModel {
+        return MachineBakedModel(bakedTextureGetter, machineHullTexture)
+    }
+}
