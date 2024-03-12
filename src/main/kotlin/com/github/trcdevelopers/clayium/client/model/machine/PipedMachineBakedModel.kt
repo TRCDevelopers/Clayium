b980b601c36064a29dbf331bbd37432a3634846f
bqc0n <b4c0nchan@gmail.com>
2024-03-12 12:35:54 +0900

    feat: add PipedMachineBakedModel

---
+++ src/main/kotlin/com/github/trcdevelopers/clayium/client/model/machine/PipedMachineBakedModel.kt
@@ -0,0 +1,27 @@
+package com.github.trcdevelopers.clayium.client.model.machine
+
+import net.minecraft.block.state.IBlockState
+import net.minecraft.client.renderer.block.model.BakedQuad
+import net.minecraft.client.renderer.block.model.IBakedModel
+import net.minecraft.client.renderer.block.model.ItemOverrideList
+import net.minecraft.client.renderer.texture.TextureAtlasSprite
+import net.minecraft.util.EnumFacing
+import net.minecraft.util.ResourceLocation
+import java.util.function.Function
+
+class PipedMachineBakedModel(
+    machineHullLocation: ResourceLocation,
+    bakedTextureGetter: Function<ResourceLocation, TextureAtlasSprite>,
+) : IBakedModel {
+    private val machineHull = bakedTextureGetter.apply(machineHullLocation)
+
+    override fun getQuads(state: IBlockState?, side: EnumFacing?, rand: Long): MutableList<BakedQuad> {
+        TODO("Not yet implemented")
+    }
+
+    override fun isAmbientOcclusion() = true
+    override fun isGui3d() = true
+    override fun isBuiltInRenderer() = false
+    override fun getParticleTexture() = machineHull
+    override fun getOverrides(): ItemOverrideList = ItemOverrideList.NONE
+}
