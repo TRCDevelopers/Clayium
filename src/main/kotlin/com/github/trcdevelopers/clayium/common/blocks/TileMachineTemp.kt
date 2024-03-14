ee5fe9a528d92b1c06ba01dd8ff8c099de10eb23
bqc0n <b4c0nchan@gmail.com>
2024-03-14 14:28:32 +0900

    feat: create TileMachineTemp (will replace TileEntityMachine)

---
+++ src/main/kotlin/com/github/trcdevelopers/clayium/common/blocks/TileMachineTemp.kt
@@ -0,0 +1,11 @@
+package com.github.trcdevelopers.clayium.common.blocks
+
+import net.minecraft.tileentity.TileEntity
+
+/**
+ * root of all tile machines
+ *
+ * - has auto IO handler
+ */
+class TileMachineTemp : TileEntity() {
+}
