52281e000dc570f57ca0ff64b68efed3b555fa3d
bqc0n <b4c0nchan@gmail.com>
2024-03-14 14:16:49 +0900

    feat: BlockMachineTemp.init

--- src/main/kotlin/com/github/trcdevelopers/clayium/common/blocks/BlockMachineTemp.kt
+++ src/main/kotlin/com/github/trcdevelopers/clayium/common/blocks/BlockMachineTemp.kt
@@ -1,6 +1,10 @@
 package com.github.trcdevelopers.clayium.common.blocks
 
+import com.github.trcdevelopers.clayium.common.Clayium
 import com.github.trcdevelopers.clayium.common.blocks.BlockMachineTemp.Companion.IS_PIPE
+import net.minecraft.block.Block
+import net.minecraft.block.SoundType
+import net.minecraft.block.material.Material
 import net.minecraft.block.properties.PropertyBool
 
 /**
@@ -8,7 +12,15 @@ import net.minecraft.block.properties.PropertyBool
  *
  * - can be piped (see [IS_PIPE])
  */
-class BlockMachineTemp {
+class BlockMachineTemp(
+
+): Block(Material.IRON) {
+    init {
+        setCreativeTab(Clayium.creativeTab)
+        setHardness(5.0f)
+        setHarvestLevel("pickaxe", 1)
+        setSoundType(SoundType.METAL)
+    }
     companion object {
         val IS_PIPE = PropertyBool.create("is_pipe")
     }
