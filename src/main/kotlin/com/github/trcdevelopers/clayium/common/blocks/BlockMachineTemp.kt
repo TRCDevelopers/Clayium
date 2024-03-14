7209d0d241d32dad5bf796a2a19e7cddc7e1e6df
bqc0n <b4c0nchan@gmail.com>
2024-03-14 18:30:17 +0900

    feat: facing and blockstates

--- src/main/kotlin/com/github/trcdevelopers/clayium/common/blocks/BlockMachineTemp.kt
+++ src/main/kotlin/com/github/trcdevelopers/clayium/common/blocks/BlockMachineTemp.kt
@@ -6,12 +6,17 @@ import net.minecraft.block.Block
 import net.minecraft.block.SoundType
 import net.minecraft.block.material.Material
 import net.minecraft.block.properties.PropertyBool
+import net.minecraft.block.properties.PropertyDirection
+import net.minecraft.block.state.BlockStateContainer
 import net.minecraft.block.state.IBlockState
 import net.minecraft.entity.EntityLiving
 import net.minecraft.util.BlockRenderLayer
+import net.minecraft.util.EnumFacing
 import net.minecraft.util.math.BlockPos
+import net.minecraft.world.ChunkCache
 import net.minecraft.world.IBlockAccess
 import net.minecraft.world.World
+import net.minecraft.world.chunk.Chunk
 
 /**
  * Root of all Clayium BlockMachines.
@@ -28,6 +33,32 @@ class BlockMachineTemp(
         setHardness(5.0f)
         setHarvestLevel("pickaxe", 1)
         setSoundType(SoundType.METAL)
+
+        defaultState = defaultState
+            .withProperty(IS_PIPE, false).withProperty(FACING_HORIZONTAL, EnumFacing.NORTH)
+    }
+
+    override fun createBlockState(): BlockStateContainer {
+        return BlockStateContainer.Builder(this)
+            .add(IS_PIPE, FACING_HORIZONTAL)
+            .build()
+    }
+
+    override fun getMetaFromState(state: IBlockState) = if (state.getValue(IS_PIPE)) 1 else 0
+    override fun getStateFromMeta(meta: Int): IBlockState = defaultState.withProperty(IS_PIPE, meta == 1)
+
+    override fun isFullBlock(state: IBlockState) = !state.getValue(IS_PIPE)
+    override fun isFullCube(state: IBlockState) = isFullBlock(state)
+    override fun isOpaqueCube(state: IBlockState) = isFullBlock(state)
+    override fun causesSuffocation(state: IBlockState) = isFullBlock(state)
+
+    override fun getActualState(state: IBlockState, worldIn: IBlockAccess, pos: BlockPos): IBlockState {
+        val tile = if (worldIn is ChunkCache) worldIn.getTileEntity(pos, Chunk.EnumCreateEntityType.CHECK) else worldIn.getTileEntity(pos)
+        if (tile is TileMachineTemp) {
+            return state.withProperty(FACING_HORIZONTAL, tile.currentFacing)
+        } else {
+            return state
+        }
     }
 
     override fun hasTileEntity(state: IBlockState) = true
@@ -37,12 +68,8 @@ class BlockMachineTemp(
 
     override fun getRenderLayer() = BlockRenderLayer.CUTOUT_MIPPED
 
-    override fun isFullBlock(state: IBlockState) = !state.getValue(IS_PIPE)
-    override fun isFullCube(state: IBlockState) = isFullBlock(state)
-    override fun isOpaqueCube(state: IBlockState) = isFullBlock(state)
-    override fun causesSuffocation(state: IBlockState) = isFullBlock(state)
-
     companion object {
         val IS_PIPE = PropertyBool.create("is_pipe")
+        val FACING_HORIZONTAL = PropertyDirection.create("facing", EnumFacing.Plane.HORIZONTAL)
     }
 }
