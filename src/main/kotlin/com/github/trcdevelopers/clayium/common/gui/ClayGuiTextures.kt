e53188326ae5661b752e611a8deb8e49bc6327cf
bqc0n <b4c0nchan@gmail.com>
2024-04-12 20:41:31 +0900

    add ClayGuiTextures

---
+++ src/main/kotlin/com/github/trcdevelopers/clayium/common/gui/ClayGuiTextures.kt
@@ -0,0 +1,20 @@
+package com.github.trcdevelopers.clayium.common.gui
+
+import com.cleanroommc.modularui.drawable.UITexture
+import com.github.trcdevelopers.clayium.common.Clayium
+
+object ClayGuiTextures {
+    val LARGE_SLOT = UITexture.builder()
+        .location(Clayium.MOD_ID, "gui/slot")
+        .imageSize(256, 256)
+        .uv(0, 32, 26, 26)
+        .canApplyTheme()
+        .build()
+
+    val PROGRESS_BAR = UITexture.builder()
+        .location(Clayium.MOD_ID, "gui/progress_bar")
+        .imageSize(256, 256)
+        .uv(1, 0, 22, 34)
+        .canApplyTheme()
+        .build()
+}
