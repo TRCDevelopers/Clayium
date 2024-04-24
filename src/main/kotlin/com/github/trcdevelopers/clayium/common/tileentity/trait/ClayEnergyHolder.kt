2ffae6c5123832109ce82a576107e105a6911c1e
bqc0n <b4c0nchan@gmail.com>
2024-04-24 13:42:41 +0900

    create ClayEnergyHolder

---
+++ src/main/kotlin/com/github/trcdevelopers/clayium/common/tileentity/trait/ClayEnergyHolder.kt
@@ -0,0 +1,63 @@
+package com.github.trcdevelopers.clayium.common.tileentity.trait
+
+import com.cleanroommc.modularui.api.widget.IWidget
+import com.cleanroommc.modularui.value.sync.SyncHandlers
+import com.cleanroommc.modularui.widgets.ItemSlot
+import com.github.trcdevelopers.clayium.common.clayenergy.ClayEnergy
+import com.github.trcdevelopers.clayium.common.clayenergy.IEnergizedClay
+import com.github.trcdevelopers.clayium.common.tileentity.TileEntityMachine
+import net.minecraft.item.ItemStack
+import net.minecraftforge.items.ItemStackHandler
+
+class ClayEnergyHolder(
+    tile: TileEntityMachine,
+    tier: Int,
+) : TETrait(tile, tier) {
+    private val slot = object : ItemStackHandler(1) {
+        override fun isItemValid(slot: Int, stack: ItemStack): Boolean {
+            return stack.item is IEnergizedClay
+        }
+
+        override fun getStackLimit(slot: Int, stack: ItemStack): Int {
+            //todo: upgradable
+            return 1
+        }
+    }
+
+    private var energy = ClayEnergy.ZERO
+
+    /***
+     * @return true if energy was drawn
+     */
+    fun drawEnergy(energy: ClayEnergy): Boolean {
+        if (hasEnoughEnergy(energy)) {
+            this.energy -= energy
+            return true
+        }
+        return false
+    }
+
+    /**
+     * tries to consume energized clay from the slot if the energy is not enough
+     */
+    fun hasEnoughEnergy(energy: ClayEnergy): Boolean {
+        if (this.energy < energy) {
+            tryConsumeEnergizedClay()
+        }
+        return this.energy >= energy
+    }
+
+    fun getSlotWidget(): IWidget {
+        return ItemSlot()
+            .slot(SyncHandlers.itemSlot(slot, 0)
+                .accessibility(false, false))
+    }
+
+    private fun tryConsumeEnergizedClay() {
+        val stack = this.slot.getStackInSlot(0)
+        if (stack.isEmpty) return
+        val item = stack.item as? IEnergizedClay ?: return
+        this.energy += item.getClayEnergy(stack)
+        this.slot.extractItem(0, 1, false)
+    }
+}
