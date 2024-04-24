eaafd3a78fdc95350cced68d697cc3a68ba48459
bqc0n <b4c0nchan@gmail.com>
2024-04-24 21:27:04 +0900

    fix UnificationEntry.toString() is incorrect

--- src/main/kotlin/com/github/trcdevelopers/clayium/common/unification/stack/UnificationEntry.kt
+++ src/main/kotlin/com/github/trcdevelopers/clayium/common/unification/stack/UnificationEntry.kt
@@ -8,6 +8,6 @@ data class UnificationEntry(
     val material: Material,
 ) {
     override fun toString(): String {
-        return "$orePrefix:${material.toUpperCamel()}"
+        return "${orePrefix.camel}${material.toUpperCamel()}"
     }
 }
