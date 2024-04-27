a9fdd215521100addf22116d3877a1998a202a55
bqc0n <b4c0nchan@gmail.com>
2024-04-27 17:34:20 +0900

    add ItemHandlerProxy

---
+++ src/main/kotlin/com/github/trcdevelopers/clayium/api/capability/impl/ItemHandlerProxy.kt
@@ -0,0 +1,46 @@
+package com.github.trcdevelopers.clayium.api.capability.impl
+
+import net.minecraft.item.ItemStack
+import net.minecraftforge.items.IItemHandler
+
+class ItemHandlerProxy(
+    private val inputInventory: IItemHandler,
+    private val outputInventory: IItemHandler,
+) : IItemHandler {
+
+    override fun getSlots(): Int {
+        return inputInventory.slots + outputInventory.slots
+    }
+
+    override fun getStackInSlot(slot: Int): ItemStack {
+        return if (slot < inputInventory.slots) {
+            inputInventory.getStackInSlot(slot)
+        } else {
+            outputInventory.getStackInSlot(slot - inputInventory.slots)
+        }
+    }
+
+    override fun insertItem(slot: Int, stack: ItemStack, simulate: Boolean): ItemStack {
+        return if (slot < inputInventory.slots) {
+            inputInventory.insertItem(slot, stack, simulate)
+        } else {
+            stack
+        }
+    }
+
+    override fun extractItem(slot: Int, amount: Int, simulate: Boolean): ItemStack {
+        return if (slot < inputInventory.slots) {
+            ItemStack.EMPTY
+        } else {
+            outputInventory.extractItem(slot - inputInventory.slots, amount, simulate)
+        }
+    }
+
+    override fun getSlotLimit(slot: Int): Int {
+        return if (slot < inputInventory.slots) {
+            inputInventory.getSlotLimit(slot)
+        } else {
+            outputInventory.getSlotLimit(slot - inputInventory.slots)
+        }
+    }
+}
