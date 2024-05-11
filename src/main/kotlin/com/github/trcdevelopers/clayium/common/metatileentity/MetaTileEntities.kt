30216f4c826406aa89513a1cc293daccbb87f1c7
bqc0n <b4c0nchan@gmail.com>
2024-05-11 14:22:23 +0900

    add simple 1->1 machines

--- src/main/kotlin/com/github/trcdevelopers/clayium/common/metatileentity/MetaTileEntities.kt
+++ src/main/kotlin/com/github/trcdevelopers/clayium/common/metatileentity/MetaTileEntities.kt
@@ -11,22 +11,52 @@ import net.minecraftforge.fml.relauncher.SideOnly
 
 object MetaTileEntities {
 
-    val CLAY_BUFFER = registerMetaTileEntities("clay_buffer", 1, (4..13)) {
+    val CLAY_BUFFER = registerMetaTileEntities(1, (4..13)) {
         ClayBufferMetaTileEntity(clayiumId("clay_buffer_tier$it"), it)
     }
 
-    val BENDING_MACHINE = registerMetaTileEntities(11, intArrayOf(1, 2, 3, 4, 5, 6, 7, 9)) {
+    val BENDING_MACHINE = registerMetaTileEntities(11, intArrayOf(1, 2, 3, 4, 5, 6, 7, 9)) { //+8
         SimpleMachineMetaTileEntity(clayiumId("bending_machine_tier$it"), it, CRecipes.BENDING)
     }
+    val CONDENSER = registerMetaTileEntities(19, intArrayOf(2, 3, 4, 5, 10)) { //+5
+        SimpleMachineMetaTileEntity(clayiumId("condenser_tier$it"), it, CRecipes.CONDENSER)
+    }
+    val CUTTING_MACHINE = registerMetaTileEntities(24, (1..4)) { //+4
+        SimpleMachineMetaTileEntity(clayiumId("cutting_machine_tier$it"), it, CRecipes.CUTTING_MACHINE)
+    }
+    val DECOMPOSER = registerMetaTileEntities(28, (2..4)) { //+3
+        SimpleMachineMetaTileEntity(clayiumId("decomposer_tier$it"), it, CRecipes.DECOMPOSER)
+    }
+    val ENERGETIC_CLAY_CONDENSER = registerMetaTileEntities(31, (3..4)) { //+2
+        SimpleMachineMetaTileEntity(clayiumId("energetic_clay_condenser_tier$it"), it, CRecipes.ENERGETIC_CLAY_CONDENSER)
+    }
+    val GRINDER = registerMetaTileEntities(33, intArrayOf(2, 3, 4, 5, 6, 10)) { //+6
+        SimpleMachineMetaTileEntity(clayiumId("grinder_tier$it"), it, CRecipes.GRINDER)
+    }
+    val LATHE = registerMetaTileEntities(39, (1..4)) { //+4
+        SimpleMachineMetaTileEntity(clayiumId("lathe_tier$it"), it, CRecipes.LATHE)
+    }
+    val MATTER_TRANSFORMER = registerMetaTileEntities(43, (7..12)) { //+6
+        SimpleMachineMetaTileEntity(clayiumId("matter_transformer_tier$it"), it, CRecipes.MATTER_TRANSFORMER)
+    }
+    val MILLING_MACHINE = registerMetaTileEntities(49, intArrayOf(1, 3, 4)) { //+3
+        SimpleMachineMetaTileEntity(clayiumId("milling_machine_tier$it"), it, CRecipes.MILLING_MACHINE)
+    }
+    val PIPE_DRAWING_MACHINE = registerMetaTileEntities(52, (1..4)) { //+4
+        SimpleMachineMetaTileEntity(clayiumId("pipe_drawing_machine_tier$it"), it, CRecipes.PIPE_DRAWING_MACHINE)
+    }
+    val WIRE_DRAWING_MACHINE = registerMetaTileEntities(56, (1..4)) { //+4
+        SimpleMachineMetaTileEntity(clayiumId("wire_drawing_machine_tier$it"), it, CRecipes.WIRE_DRAWING_MACHINE)
+    }
 
-    val ASSEMBLER = registerMetaTileEntities(19, intArrayOf(3, 4, 6, 10)) {
+    val ASSEMBLER = registerMetaTileEntities(60, intArrayOf(3, 4, 6, 10)) { //+4
         SimpleMachineMetaTileEntity(clayiumId("assembler_tier$it"), it, CRecipes.ASSEMBLER)
     }
 
     /**
      * @param provider tier -> MetaTileEntity
      */
-    fun <T : MetaTileEntity> registerMetaTileEntities(name: String, startId: Int, tiers: IntRange, provider: (Int) -> T): List<T> {
+    fun <T : MetaTileEntity> registerMetaTileEntities(startId: Int, tiers: IntRange, provider: (Int) -> T): List<T> {
         return registerMetaTileEntities(startId, tiers.toList().toIntArray(), provider)
     }
 
--- src/main/kotlin/com/github/trcdevelopers/clayium/common/recipe/registry/CRecipes.kt
+++ src/main/kotlin/com/github/trcdevelopers/clayium/common/recipe/registry/CRecipes.kt
@@ -7,8 +7,31 @@ object CRecipes {
     private val REGISTRY = mutableMapOf<String, RecipeRegistry<*>>()
     val ALL_REGISTRIES: Map<String, RecipeRegistry<*>> get() = REGISTRY.toMap()
 
+    // 1 -> 1 recipes
     val BENDING = addRegistry("bending_machine", SimpleRecipeBuilder(), inputSize = 1, outputSize = 1)
+    val CONDENSER = addRegistry("condenser", SimpleRecipeBuilder(), 1, 1)
+    val CUTTING_MACHINE = addRegistry("cutting_machine", SimpleRecipeBuilder(), 1, 1)
+    val DECOMPOSER = addRegistry("decomposer", SimpleRecipeBuilder(), 1, 1)
+    val ENERGETIC_CLAY_CONDENSER = addRegistry("energetic_clay_condenser", SimpleRecipeBuilder(), 1, 1)
+    val GRINDER = addRegistry("grinder", SimpleRecipeBuilder(), 1, 1)
+    val LATHE = addRegistry("lathe", SimpleRecipeBuilder(), 1, 1)
+    val MATTER_TRANSFORMER = addRegistry("matter_transformer", SimpleRecipeBuilder(), 1, 1)
+    val MILLING_MACHINE = addRegistry("milling_machine", SimpleRecipeBuilder(), 1, 1)
+    val PIPE_DRAWING_MACHINE = addRegistry("pipe_drawing_machine", SimpleRecipeBuilder(), 1, 1)
+    val WIRE_DRAWING_MACHINE = addRegistry("wire_drawing_machine", SimpleRecipeBuilder(), 1, 1)
+    /**
+     * todo: (smelter, solar clay fabricator) are maybe special
+     * todo: electrolysis reactor can be dynamic ore-prefixed recipe
+     * todo: energetic clay decomposer
+     */
+
+    // 2 -> 1 recipes
     val ASSEMBLER = addRegistry("assembler", SimpleRecipeBuilder(), 2, 1)
+    val INSCRIBER = addRegistry("inscriber", SimpleRecipeBuilder(), 2, 1)
+    val ALLOT_SMELTER = addRegistry("allot_smelter", SimpleRecipeBuilder(), 2, 1)
+
+    // others
+    val CHEMICAL_REACTOR = addRegistry("chemical_reactor", SimpleRecipeBuilder(), 2, 2)
 
     fun <R: RecipeBuilder<R>> addRegistry(name: String, buildSample: R, inputSize: Int, outputSize: Int): RecipeRegistry<R> {
         val registry = RecipeRegistry(name, buildSample, inputSize, outputSize)
