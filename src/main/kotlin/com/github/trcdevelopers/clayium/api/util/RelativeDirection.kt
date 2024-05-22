5bec2714653fd3e4c513e962c7c3c1f5a47f3138
bqc0n <b4c0nchan@gmail.com>
2024-05-22 13:21:01 +0900

    create a Relative version of EnumFacing

---
+++ src/main/kotlin/com/github/trcdevelopers/clayium/api/util/RelativeDirection.kt
@@ -0,0 +1,23 @@
+package com.github.trcdevelopers.clayium.api.util
+
+import net.minecraft.util.EnumFacing
+
+enum class RelativeDirection(
+    private val actualFacingGetter: (frontFacing: EnumFacing) -> EnumFacing
+) {
+    UP({ EnumFacing.UP }),
+    DOWN({ EnumFacing.DOWN }),
+    LEFT(EnumFacing::rotateY),
+    RIGHT(EnumFacing::rotateYCCW),
+    FRONT({ it }),
+    BACK({ it.opposite });
+
+    /**
+     * @param frontFacing must be horizontal.
+     * @throws IllegalArgumentException if frontFacing is not horizontal.
+     */
+    fun getActualFacing(frontFacing: EnumFacing): EnumFacing {
+        require(frontFacing.axis.isHorizontal) { "Front facing must be horizontal" }
+        return actualFacingGetter(frontFacing)
+    }
+}
