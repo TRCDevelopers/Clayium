fa37e6ae49833f52a45858e439e1a8087424093e
bqc0n <b4c0nchan@gmail.com>
2024-05-31 10:05:27 +0900

    fix laser energy calculation

--- src/main/kotlin/com/github/trcdevelopers/clayium/api/laser/ClayLaser.kt
+++ src/main/kotlin/com/github/trcdevelopers/clayium/api/laser/ClayLaser.kt
@@ -11,9 +11,9 @@ data class ClayLaser(
     override val laserAge: Int = 0,
 ) : IClayLaser {
 
-    override val laserEnergy: Double = calcLaserEnergyPerColor(laserBlue, bases[0], maxEnergies[0], dampingRates[0]) *
-                                       calcLaserEnergyPerColor(laserGreen, bases[1], maxEnergies[1], dampingRates[1]) *
-                                       calcLaserEnergyPerColor(laserRed, bases[2], maxEnergies[2], dampingRates[2]) - 1
+    override val laserEnergy: Double = calcLaserEnergyPerColor(laserBlue, bases[0], maxEnergies[0]) *
+                                       calcLaserEnergyPerColor(laserGreen, bases[1], maxEnergies[1]) *
+                                       calcLaserEnergyPerColor(laserRed, bases[2], maxEnergies[2]) - 1
 
     fun changeDirection(direction: EnumFacing): ClayLaser {
         return ClayLaser(direction, laserRed, laserGreen, laserBlue)
@@ -22,20 +22,19 @@ data class ClayLaser(
     companion object {
         private val bases = doubleArrayOf(2.5, 1.8, 1.5)
         private val maxEnergies = doubleArrayOf(1000.0, 300.0, 100.0)
-        private val dampingRates = doubleArrayOf(0.1, 0.1, 0.1)
 
-        /**
-         * the calculation is found [here](https://clayium.wiki.fc2.com/wiki/%E3%83%AC%E3%83%BC%E3%82%B6%E3%83%BC)
-         * todo: wikiと計算結果が異なるので実際の動作確認が必要
-         */
-        private fun calcLaserEnergyPerColor(laserStrength: Int, base: Double, maxEnergy: Double, r: Double): Double {
-            if (laserStrength <= 0 || r <= 0.0 || maxEnergy < 0.0 || base  < 1.0) return 1.0
-            val laserStrengthd = laserStrength.toDouble()
-            val c = Math.log(maxEnergy) / Math.log((1.0 + r) / r) * Math.log(base)
-            val a = Math.pow(Math.E, (1.0 + r) / c)
-            val e = Math.pow(maxEnergy, Math.log((1.0 + r) / (Math.pow(a, -laserStrengthd) + r)) / Math.log((1.0 + r) / r))
-            val m = 1.0 / (1.0 + r * Math.pow(a, laserStrengthd)) + r * Math.pow(a, laserStrengthd) / (1.0 + r * Math.pow(a, laserStrengthd)) * laserStrengthd
-            return max(e * m, 1.0)
+        @Suppress("LocalVariableName")
+        private fun calcLaserEnergyPerColor(n_i: Int, b_i: Double, m_i: Double, r: Double = 0.1): Double {
+            if (n_i <= 0 || r <= 0.0 || m_i < 0.0 || b_i  < 1.0) return 1.0
+            val nd = n_i.toDouble()
+            val r1 = r + 1.0
+            val C_i = Math.pow(b_i, (r1) * (Math.log(r1 / r) / Math.log(m_i)))
+            val ai_top = Math.log(r1 / (Math.pow(C_i, -nd) + r))
+            val ai_bottom = Math.log(r1 / r)
+            val ai = ai_top / ai_bottom
+
+            val E_i = Math.pow(m_i, ai) * ((1 + r * n_i * Math.pow(C_i, nd)) / (1 + r * Math.pow(C_i, nd)))
+            return max(1.0, E_i)
         }
     }
 }
