e324885092bffedcfe6cdd84f6a6ec3cd6ab05bb
bqc0n <b4c0nchan@gmail.com>
2024-07-12 14:02:52 +0900

    feat: recipe validation

--- src/main/kotlin/com/github/trcdevelopers/clayium/common/recipe/RecipeUtils.kt
+++ src/main/kotlin/com/github/trcdevelopers/clayium/common/recipe/RecipeUtils.kt
@@ -3,10 +3,10 @@ package com.github.trcdevelopers.clayium.common.recipe
 import com.github.trcdevelopers.clayium.api.util.clayiumId
 import com.github.trcdevelopers.clayium.common.Clayium
 import com.github.trcdevelopers.clayium.common.items.metaitem.MetaItemClayium
-import com.github.trcdevelopers.clayium.common.unification.OreDictUnifier
 import com.github.trcdevelopers.clayium.common.unification.stack.UnificationEntry
 import net.minecraft.item.ItemStack
 import net.minecraft.item.crafting.FurnaceRecipes
+import net.minecraft.util.ResourceLocation
 import net.minecraftforge.fml.common.registry.ForgeRegistries
 import net.minecraftforge.oredict.ShapedOreRecipe
 import net.minecraftforge.oredict.ShapelessOreRecipe
@@ -28,9 +28,13 @@ object RecipeUtils {
     }
 
     fun addShapedRecipe(registryName: String, result: ItemStack, isMirrored: Boolean, vararg recipe: Any) {
-        ForgeRegistries.RECIPES.register(ShapedOreRecipe(null, result, *finalizeRecipe(recipe))
-            .setMirrored(isMirrored)
-            .setRegistryName(clayiumId(registryName)))
+        val registryName = clayiumId(registryName)
+        if (validateRecipe(registryName, result, recipe)) {
+            ForgeRegistries.RECIPES.register(
+                ShapedOreRecipe(null, result, *finalizeRecipe(recipe))
+                    .setMirrored(isMirrored)
+                    .setRegistryName(registryName))
+        }
     }
 
     fun addShapelessRecipe(registryName: String, result: ItemStack, vararg recipe: Any) {
@@ -52,6 +56,18 @@ object RecipeUtils {
             is UnificationEntry -> ingredient.toString()
             else -> ingredient
         }
+    }
+
+    private fun validateRecipe(registryName: ResourceLocation, result: ItemStack, recipe: Array<out Any>): Boolean {
+        if (ForgeRegistries.RECIPES.containsKey(registryName)) {
+            logInvalidRecipe("Recipe $registryName already exists."); return false
+        } else if (recipe.isEmpty()) {
+            logInvalidRecipe("Recipe $registryName has no ingredients."); return false
+        } else if (result.isEmpty) {
+            logInvalidRecipe("Recipe $registryName has an empty result."); return false
+        }
+
+        return true
     }
 
     fun logInvalidRecipe(msg: String) {
