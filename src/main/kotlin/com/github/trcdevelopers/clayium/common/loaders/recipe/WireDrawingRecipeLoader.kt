db3f958bde003828d1cad204fa5c74c37b3fa4f1
bqc0n <b4c0nchan@gmail.com>
2024-07-12 12:49:48 +0900

    fix: WireDrawingRecipeLoader registers recipe with duration 0

--- src/main/kotlin/com/github/trcdevelopers/clayium/common/loaders/recipe/WireDrawingRecipeLoader.kt
+++ src/main/kotlin/com/github/trcdevelopers/clayium/common/loaders/recipe/WireDrawingRecipeLoader.kt
@@ -19,21 +19,21 @@ object WireDrawingRecipeLoader {
 
         for ((i, material) in listOf(CMaterials.clay, CMaterials.denseClay).withIndex()) {
             registry.builder()
-                .input(OrePrefix.pipe, material)
-                .output(OrePrefix.stick, material, 4)
-                .tier(0).CEt(ClayEnergy.micro(10)).duration(1 * i)
+                .input(OrePrefix.smallDisc, material)
+                .output(OrePrefix.stick, material)
+                .tier(0).CEt(ClayEnergy.micro(10)).duration(1 * (i + 1))
                 .buildAndRegister()
 
             registry.builder()
-                .input(OrePrefix.smallDisc, material)
-                .output(OrePrefix.stick, material)
-                .tier(0).CEt(ClayEnergy.micro(10)).duration(1 * i)
+                .input(OrePrefix.pipe, material)
+                .output(OrePrefix.stick, material, 4)
+                .tier(0).CEt(ClayEnergy.micro(10)).duration(1 * (i + 1))
                 .buildAndRegister()
 
             registry.builder()
                 .input(OrePrefix.cylinder, material)
                 .output(OrePrefix.stick, material, 8)
-                .tier(0).CEt(ClayEnergy.micro(10)).duration(3* i)
+                .tier(0).CEt(ClayEnergy.micro(10)).duration(3 * (i + 1))
                 .buildAndRegister()
         }
     }
