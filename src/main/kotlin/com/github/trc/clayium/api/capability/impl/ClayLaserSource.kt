ae66eee4e393d4becdc75b83c30d1564e34b65a3
soramame0256 <62879324+soramame0256@users.noreply.github.com>
2024-07-18 11:11:53 +0900

    fix: resolves (#79)

--- src/main/kotlin/com/github/trc/clayium/api/capability/impl/ClayLaserSource.kt
+++ src/main/kotlin/com/github/trc/clayium/api/capability/impl/ClayLaserSource.kt
@@ -141,8 +141,9 @@ class ClayLaserSource(
             previousTarget?.takeUnless { it.isInvalid }
                 ?.getCapability(ClayiumTileCapabilities.CAPABILITY_CLAY_LASER_ACCEPTOR, targetSide)
                 ?.laserChanged(targetSide, null)
-            laserTarget?.getCapability(ClayiumTileCapabilities.CAPABILITY_CLAY_LASER_ACCEPTOR, targetSide)
-                ?.laserChanged(targetSide, this.laser)
+            if (isActive)
+                laserTarget?.getCapability(ClayiumTileCapabilities.CAPABILITY_CLAY_LASER_ACCEPTOR, targetSide)
+                    ?.laserChanged(targetSide, this.laser)
             writeLaserData()
         }
     }
