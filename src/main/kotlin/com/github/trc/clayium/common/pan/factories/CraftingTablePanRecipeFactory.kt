ce90570cf21c30e04b24626100601b6ddd8f1d33
bqc0n <b4c0nchan@gmail.com>
2024-07-24 11:15:59 +0900

    feat: fuzzy inputs for CraftingTablePanRecipeFactory

--- src/main/kotlin/com/github/trc/clayium/common/pan/factories/CraftingTablePanRecipeFactory.kt
+++ src/main/kotlin/com/github/trc/clayium/common/pan/factories/CraftingTablePanRecipeFactory.kt
@@ -6,27 +6,36 @@ import com.github.trc.clayium.api.util.copyWithSize
 import com.github.trc.clayium.common.clayenergy.ClayEnergy
 import com.github.trc.clayium.common.pan.PanRecipe
 import com.github.trc.clayium.common.recipe.ingredient.CItemRecipeInput
+import com.github.trc.clayium.common.util.DummyContainer
 import net.minecraft.entity.player.EntityPlayer
 import net.minecraft.init.Blocks
 import net.minecraft.inventory.Container
 import net.minecraft.inventory.InventoryCrafting
 import net.minecraft.item.ItemStack
 import net.minecraft.item.crafting.CraftingManager
+import net.minecraft.item.crafting.IRecipe
 import net.minecraft.util.math.BlockPos
 import net.minecraft.world.IBlockAccess
 import net.minecraft.world.World
+import net.minecraftforge.oredict.OreIngredient
 
 object CraftingTablePanRecipeFactory : IPanRecipeFactory {
     override fun getEntry(world: IBlockAccess, pos: BlockPos, stacks: List<ItemStack>): IPanRecipe? {
         if (world.getBlockState(pos).block === Blocks.CRAFTING_TABLE || world !is World) return null
 
-        val dummyContainer = object : Container() { override fun canInteractWith(playerIn: EntityPlayer) = false }
-        val matrix = InventoryCrafting(dummyContainer, 3, 3)
+        val matrix = InventoryCrafting(DummyContainer, 3, 3)
         for (slot in 0..<9) matrix.setInventorySlotContents(slot, stacks[slot])
 
-        val output = CraftingManager.findMatchingResult(matrix, world)
-        if (output.isEmpty) return null
-        val inputs = stacks.filterNot(ItemStack::isEmpty).map { it.copyWithSize(1) }.map { CItemRecipeInput(listOf(it), 1) }
+        val recipe: IRecipe = CraftingManager.findMatchingRecipe(matrix, world) ?: return null
+        val output = recipe.recipeOutput
+        val inputs = recipe.ingredients.map { ingredient ->
+            // field `matchingStacks` and method `getMatchingStacks` is not the same.
+            // `matchingStacks` is an empty list in OreIngredient.
+            val stacks = ingredient.getMatchingStacks()
+            // todo: use cached recipe inputs instead of creating new one
+            CItemRecipeInput(stacks.map { it.copyWithSize(1) }, 1)
+        }
+
         return PanRecipe(inputs, listOf(output), ClayEnergy.micro(10))
     }
 }
