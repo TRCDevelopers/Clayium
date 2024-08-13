32100adcf99b9c71fb5a86bb950bb279ea01ae27
bqc0n <b4c0nchan@gmail.com>
2024-08-13 18:23:30 +0900

    fix: EStorage Upgrade texture

--- src/main/kotlin/com/github/trc/clayium/common/blocks/BlockEnergyStorageUpgrade.kt
+++ src/main/kotlin/com/github/trc/clayium/common/blocks/BlockEnergyStorageUpgrade.kt
@@ -6,13 +6,14 @@ import com.github.trc.clayium.api.block.VariantBlock
 import net.minecraft.block.material.Material
 import net.minecraft.client.util.ITooltipFlag
 import net.minecraft.item.ItemStack
+import net.minecraft.util.BlockRenderLayer
 import net.minecraft.util.math.BlockPos
 import net.minecraft.world.IBlockAccess
 import net.minecraft.world.World
 
 class BlockEnergyStorageUpgrade : VariantBlock<BlockOverclocker.BlockType>(Material.IRON), IEnergyStorageUpgradeBlock, ITieredBlock {
 
-    override fun getExtraStackLimit(world: IBlockAccess, pos: BlockPos) = getMultiplier(getEnum(world.getBlockState(pos)))
+    override fun getExtraStackLimit(world: IBlockAccess, pos: BlockPos) = getExtraStackLimit(getEnum(world.getBlockState(pos)))
 
     override fun getTier(stack: ItemStack) = getEnum(stack).tier
     override fun getTier(world: IBlockAccess, pos: BlockPos) = getEnum(world.getBlockState(pos)).tier
@@ -21,7 +22,7 @@ class BlockEnergyStorageUpgrade : VariantBlock<BlockOverclocker.BlockType>(Mater
         super.addInformation(stack, worldIn, tooltip, flagIn)
     }
 
-    private fun getMultiplier(type: BlockOverclocker.BlockType): Int {
+    private fun getExtraStackLimit(type: BlockOverclocker.BlockType): Int {
         return when (type) {
             BlockOverclocker.BlockType.ANTIMATTER -> 2
             BlockOverclocker.BlockType.PURE_ANTIMATTER -> 4
@@ -29,4 +30,6 @@ class BlockEnergyStorageUpgrade : VariantBlock<BlockOverclocker.BlockType>(Mater
             BlockOverclocker.BlockType.OPA -> 64
         }
     }
+
+    override fun getRenderLayer() = BlockRenderLayer.CUTOUT_MIPPED
 }
