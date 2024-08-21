dc0aa18fd870394b681aa672dbac11ef3d7124b2
bqc0n <89625049+bqc0n@users.noreply.github.com>
2024-08-21 22:03:10 +0900

    feat: add missing recipes

--- src/main/kotlin/com/github/trc/clayium/common/loaders/recipe/GrinderRecipeLoader.kt
+++ src/main/kotlin/com/github/trc/clayium/common/loaders/recipe/GrinderRecipeLoader.kt
@@ -1,10 +1,12 @@
 package com.github.trc.clayium.common.loaders.recipe
 
+import com.github.trc.clayium.api.ClayEnergy
 import com.github.trc.clayium.api.unification.material.CMaterials
 import com.github.trc.clayium.api.unification.ore.OrePrefix
 import com.github.trc.clayium.common.blocks.ClayiumBlocks
 import com.github.trc.clayium.common.items.metaitem.MetaItemClayParts
 import com.github.trc.clayium.common.recipe.registry.CRecipes
+import net.minecraft.init.Blocks
 
 object GrinderRecipeLoader {
     fun registerRecipes() {
@@ -29,6 +31,22 @@ object GrinderRecipeLoader {
             .input(ClayiumBlocks.CLAY_TREE_LOG)
             .output(OrePrefix.dust, CMaterials.organicClay)
             .tier(5).defaultCEt().duration(200)
+            .buildAndRegister()
+
+        registry.builder()
+            .input(Blocks.COBBLESTONE)
+            .output(Blocks.GRAVEL)
+            .tier(0).CEt(ClayEnergy.micro(10)).duration(10)
+            .buildAndRegister()
+        registry.builder()
+            .input(Blocks.COBBLESTONE, 16)
+            .output(Blocks.GRAVEL, 16)
+            .tier(1).CEt(ClayEnergy.micro(10)).duration(10)
+            .buildAndRegister()
+        registry.builder()
+            .input(Blocks.COBBLESTONE, 64)
+            .output(Blocks.GRAVEL, 64)
+            .tier(2).CEt(ClayEnergy.micro(10)).duration(10)
             .buildAndRegister()
     }
 }
