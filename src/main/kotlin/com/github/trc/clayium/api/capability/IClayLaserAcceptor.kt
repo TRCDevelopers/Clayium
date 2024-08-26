1acd1b21bd7246c6019b0d83c0098fe83875221d
bqc0n <89625049+bqc0n@users.noreply.github.com>
2024-08-26 23:15:17 +0900

    fix: state.getValue is called after the block has been destroyed

--- src/main/kotlin/com/github/trc/clayium/api/capability/IClayLaserAcceptor.kt
+++ src/main/kotlin/com/github/trc/clayium/api/capability/IClayLaserAcceptor.kt
@@ -6,6 +6,9 @@ import net.minecraft.util.EnumFacing
 interface IClayLaserAcceptor {
     /**
      * Called when the side is irradiated with a laser or when there is a change in the irradiated laser.
+     *
+     * **Note:** Can be called after the block has been destroyed.
+     *
      * @param irradiatedSide the side that is irradiated by the laser
      * @param laser if null, it means the laser irradiation has stopped.
      */
