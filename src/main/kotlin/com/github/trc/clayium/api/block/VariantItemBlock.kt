d196eb0ff9a51ec28433631fab3b6fee25f734d5
bqc0n <89625049+bqc0n@users.noreply.github.com>
2024-08-29 22:29:39 +0900

    fix: don't override getItemStackDisplayName

--- src/main/kotlin/com/github/trc/clayium/api/block/VariantItemBlock.kt
+++ src/main/kotlin/com/github/trc/clayium/api/block/VariantItemBlock.kt
@@ -1,6 +1,5 @@
 package com.github.trc.clayium.api.block
 
-import net.minecraft.client.resources.I18n
 import net.minecraft.item.ItemBlock
 import net.minecraft.item.ItemStack
 import net.minecraft.util.IStringSerializable
@@ -18,11 +17,5 @@ where E : Enum<E>, E : IStringSerializable, B : VariantBlock<E>
         val superKey = super.getTranslationKey(stack)
         val variantName = variantBlock.getEnum(stack).getName()
         return "$superKey.$variantName"
-    }
-
-    override fun getItemStackDisplayName(stack: ItemStack): String {
-        val superKey = super.getTranslationKey(stack)
-        val variantName = variantBlock.getEnum(stack).getName()
-        return I18n.format("$superKey.$variantName")
     }
 }
