a37fafa05d0d543100a0bc41cce6e3b3d29f8fac
bqc0n <89625049+bqc0n@users.noreply.github.com>
2024-08-31 12:26:36 +0900

    fix: centrifuge gui

--- src/main/kotlin/com/github/trc/clayium/common/metatileentities/CentrifugeMetaTileEntity.kt
+++ src/main/kotlin/com/github/trc/clayium/common/metatileentities/CentrifugeMetaTileEntity.kt
@@ -1,22 +1,25 @@
 package com.github.trc.clayium.common.metatileentities
 
+import com.cleanroommc.modularui.api.drawable.IKey
 import com.cleanroommc.modularui.factory.PosGuiData
 import com.cleanroommc.modularui.screen.ModularPanel
 import com.cleanroommc.modularui.utils.Alignment
 import com.cleanroommc.modularui.value.sync.GuiSyncManager
 import com.cleanroommc.modularui.value.sync.SyncHandlers
+import com.cleanroommc.modularui.widget.ParentWidget
 import com.cleanroommc.modularui.widgets.ItemSlot
 import com.cleanroommc.modularui.widgets.SlotGroupWidget
-import com.cleanroommc.modularui.widgets.layout.Column
 import com.cleanroommc.modularui.widgets.layout.Row
 import com.github.trc.clayium.api.capability.impl.RecipeLogicEnergy
 import com.github.trc.clayium.api.metatileentity.MetaTileEntity
 import com.github.trc.clayium.api.metatileentity.WorkableMetaTileEntity
 import com.github.trc.clayium.api.metatileentity.trait.AutoIoHandler
 import com.github.trc.clayium.api.util.ITier
+import com.github.trc.clayium.api.util.asWidgetResizing
 import com.github.trc.clayium.api.util.clayiumId
 import com.github.trc.clayium.common.config.ConfigTierBalance
 import com.github.trc.clayium.common.recipe.registry.CRecipes
+import net.minecraft.client.resources.I18n
 import net.minecraft.util.ResourceLocation
 
 class CentrifugeMetaTileEntity(
@@ -33,6 +36,13 @@ class CentrifugeMetaTileEntity(
         .setEnergyConsumingMultiplier(ConfigTierBalance.crafting::getConsumingEnergyMultiplier)
 
     override fun buildUI(data: PosGuiData, syncManager: GuiSyncManager): ModularPanel {
+        return ModularPanel.defaultPanel(this.metaTileEntityId.toString(), 176, 104 + ((outputSize + 1) * 9 + 46))
+            .columnWithPlayerInv {
+                child(buildMainParentWidget(syncManager))
+            }
+    }
+
+    override fun buildMainParentWidget(syncManager: GuiSyncManager): ParentWidget<*> {
         val slotsAndProgressBar = Row()
             .widthRel(0.7f).height(26)
             .align(Alignment.Center)
@@ -51,12 +61,19 @@ class CentrifugeMetaTileEntity(
             .align(Alignment.CenterRight)
         )
 
-        return ModularPanel.defaultPanel(this.metaTileEntityId.toString(), 176, 104 + ((outputSize + 1) * 9 + 46))
-            .child(Column().margin(7).sizeRel(1f)
-                .child(buildMainParentWidget(syncManager)
-                    .child(slotsAndProgressBar.align(Alignment.Center)))
-                .child(SlotGroupWidget.playerInventory(0))
-            )
+        @Suppress("DuplicatedCode") // special output slot layout
+        return ParentWidget().widthRel(1f).expanded().marginBottom(2)
+            .child(IKey.str(getStackForm().displayName).asWidget()
+                .align(Alignment.TopLeft))
+            .child(IKey.lang("container.inventory").asWidget().align(Alignment.BottomLeft))
+            .child(IKey.dynamic {
+                if (overclock != 1.0) I18n.format("gui.clayium.overclock", overclock) else " "
+            }.asWidgetResizing().alignment(Alignment.CenterRight).align(Alignment.BottomRight))
+            .child(slotsAndProgressBar.align(Alignment.Center))
+            .child(clayEnergyHolder.createCeTextWidget(syncManager)
+                .bottom(12).left(0))
+            .child(clayEnergyHolder.createSlotWidget()
+                .align(Alignment.BottomRight))
     }
 
     override fun createMetaTileEntity(): MetaTileEntity {
