b593803136c58501389167fb9b7077a73d16891a
bqc0n <89625049+bqc0n@users.noreply.github.com>
2024-09-07 16:03:03 +0900

    fix: phantom slot dupe issue with mixin

---
+++ src/main/java/com/github/trc/clayium/mixins/modularui/FixDupeWithPhantomSlotMixin.java
@@ -0,0 +1,36 @@
+package com.github.trc.clayium.mixins.modularui;
+
+import com.cleanroommc.modularui.screen.ModularContainer;
+import com.cleanroommc.modularui.widgets.slot.ModularSlot;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.ClickType;
+import net.minecraft.item.ItemStack;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.List;
+
+//todo: delete this once it's fixed in ModularUI.
+// open a PR to ModularUI to fix this.
+@Mixin(ModularContainer.class)
+public abstract class FixDupeWithPhantomSlotMixin {
+
+    @Final @Shadow private List<ModularSlot> slots;
+
+    @Inject(method = "slotClick", at = @At("HEAD"), cancellable = true)
+    public void clayium$slotClick(int slotId, int mouseButton, ClickType clickTypeIn, EntityPlayer player, CallbackInfoReturnable<ItemStack> cir) {
+        if (!(clickTypeIn == ClickType.SWAP && mouseButton >= 0 && mouseButton < 9)) return;
+        ModularSlot hotbar = this.slots.get(mouseButton);
+        ModularSlot phantom = this.slots.get(slotId);
+        if (!phantom.isPhantom()) return;
+
+        phantom.putStack(hotbar.getStack());
+
+        cir.setReturnValue(ItemStack.EMPTY);
+        cir.cancel();
+    }
+}
