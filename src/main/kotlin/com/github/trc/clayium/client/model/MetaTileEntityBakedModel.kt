2c52000e49b56e1b10f25a2c73022dc604fa9dba
べーこん <89625049+bqc0n@users.noreply.github.com>
2024-09-08 20:01:26 +0900

    fix: machine IO overlays aren't culled (#167)

--- src/main/kotlin/com/github/trc/clayium/client/model/MetaTileEntityBakedModel.kt
+++ src/main/kotlin/com/github/trc/clayium/client/model/MetaTileEntityBakedModel.kt
@@ -4,71 +4,18 @@ import codechicken.lib.render.particle.IModelParticleProvider
 import codechicken.lib.texture.TextureUtils
 import com.github.trc.clayium.api.block.BlockMachine.Companion.TILE_ENTITY
 import com.github.trc.clayium.api.metatileentity.MetaTileEntityHolder
-import com.github.trc.clayium.api.util.MachineIoMode
-import com.github.trc.clayium.api.util.MachineIoMode.*
-import com.github.trc.clayium.api.util.clayiumId
 import com.github.trc.clayium.api.util.getMetaTileEntity
 import net.minecraft.block.state.IBlockState
 import net.minecraft.client.renderer.block.model.BakedQuad
 import net.minecraft.client.renderer.block.model.ItemOverrideList
 import net.minecraft.client.renderer.texture.TextureAtlasSprite
 import net.minecraft.util.EnumFacing
-import net.minecraft.util.ResourceLocation
 import net.minecraft.util.math.BlockPos
 import net.minecraft.util.math.RayTraceResult
 import net.minecraft.world.IBlockAccess
 import net.minecraftforge.common.property.IExtendedBlockState
 
-class MetaTileEntityBakedModel(
-    bakedTextureGetter: java.util.function.Function<ResourceLocation, TextureAtlasSprite>,
-) : IModelParticleProvider {
-
-    private val inputModeQuads: Map<MachineIoMode, List<BakedQuad>?> = MachineIoMode.entries.associateWith { ioMode ->
-        val textureName = when (ioMode) {
-            NONE -> return@associateWith null
-            FIRST -> "import_1"
-            SECOND -> "import_2"
-            ALL -> "import"
-            CE -> "import_energy"
-            M_ALL -> "import_m0"
-            M_1 -> "import_m1"
-            M_2 -> "import_m2"
-            M_3 -> "import_m3"
-            M_4 -> "import_m4"
-            M_5 -> "import_m5"
-            M_6 -> "import_m6"
-        }
-        val atlasSprite = bakedTextureGetter.apply(clayiumId("blocks/$textureName"))
-        EnumFacing.entries.map { side ->
-            ModelTextures.createQuad(side, atlasSprite)
-        }
-    }
-
-    private val outputModeQuads: Map<MachineIoMode, List<BakedQuad>?> = MachineIoMode.entries.associateWith { ioMode ->
-        val textureName = when (ioMode) {
-            NONE -> return@associateWith null
-            FIRST -> "export_1"
-            SECOND -> "export_2"
-            ALL -> "export"
-            CE -> return@associateWith null
-            M_ALL -> "export_m0"
-            M_1 -> "export_m1"
-            M_2 -> "export_m2"
-            M_3 -> "export_m3"
-            M_4 -> "export_m4"
-            M_5 -> "export_m5"
-            M_6 -> "export_m6"
-        }
-        val atlasSprite = bakedTextureGetter.apply(clayiumId("blocks/$textureName"))
-        EnumFacing.entries.map { side ->
-            ModelTextures.createQuad(side, atlasSprite)
-        }
-    }
-
-    private val filterQuads = EnumFacing.entries.map { side ->
-        val atlasSprite = bakedTextureGetter.apply(clayiumId("blocks/filter"))
-        ModelTextures.createQuad(side, atlasSprite)
-    }
+class MetaTileEntityBakedModel : IModelParticleProvider {
 
     override fun getQuads(state: IBlockState?, side: EnumFacing?, rand: Long): List<BakedQuad> {
         if (state == null || side == null || state !is IExtendedBlockState) return emptyList()
@@ -77,17 +24,9 @@ class MetaTileEntityBakedModel(
         val quads = mutableListOf<BakedQuad>()
         mte.getQuads(quads, state, side, rand)
         mte.overlayQuads(quads, state, side, rand)
-        mte.inputModes.forEachIndexed { facingIndex, mteInputMode ->
-            val side2Quad = inputModeQuads[mteInputMode] ?: return@forEachIndexed
-            quads.add(side2Quad[facingIndex])
-        }
-        mte.outputModes.forEachIndexed { facingIndex, mteOutputMode ->
-            val side2Quad = outputModeQuads[mteOutputMode] ?: return@forEachIndexed
-            quads.add(side2Quad[facingIndex])
-        }
-        mte.filters.forEachIndexed { i, filter ->
-            if (filter != null) quads.add(filterQuads[i])
-        }
+        ModelTextures.getInputQuad(mte.inputModes[side.index], side)?.let { quads.add(it) }
+        ModelTextures.getOutputQuad(mte.outputModes[side.index], side)?.let { quads.add(it) }
+        mte.filters[side.index]?.let { quads.add(ModelTextures.getFilterQuad(side)) }
         return quads
     }
 
--- src/main/kotlin/com/github/trc/clayium/client/model/ModelTextures.kt
+++ src/main/kotlin/com/github/trc/clayium/client/model/ModelTextures.kt
@@ -3,6 +3,20 @@ package com.github.trc.clayium.client.model
 import com.github.trc.clayium.api.ClayiumApi
 import com.github.trc.clayium.api.util.ClayTiers
 import com.github.trc.clayium.api.util.ITier
+import com.github.trc.clayium.api.util.MachineIoMode
+import com.github.trc.clayium.api.util.MachineIoMode.ALL
+import com.github.trc.clayium.api.util.MachineIoMode.CE
+import com.github.trc.clayium.api.util.MachineIoMode.FIRST
+import com.github.trc.clayium.api.util.MachineIoMode.M_1
+import com.github.trc.clayium.api.util.MachineIoMode.M_2
+import com.github.trc.clayium.api.util.MachineIoMode.M_3
+import com.github.trc.clayium.api.util.MachineIoMode.M_4
+import com.github.trc.clayium.api.util.MachineIoMode.M_5
+import com.github.trc.clayium.api.util.MachineIoMode.M_6
+import com.github.trc.clayium.api.util.MachineIoMode.M_ALL
+import com.github.trc.clayium.api.util.MachineIoMode.NONE
+import com.github.trc.clayium.api.util.MachineIoMode.SECOND
+import com.github.trc.clayium.api.util.clayiumId
 import net.minecraft.client.renderer.block.model.BakedQuad
 import net.minecraft.client.renderer.block.model.BlockFaceUV
 import net.minecraft.client.renderer.block.model.BlockPartFace
@@ -25,13 +39,14 @@ object ModelTextures {
     lateinit var MISSING: TextureAtlasSprite
         private set
 
-    private lateinit var HULL_QUADS: Map<String, Map<EnumFacing, BakedQuad>>
-
-    private lateinit var HULL_TEXTURES: Map<String, TextureAtlasSprite>
-
     // metaTileEntity.faceTexture -> map
     private val _faceQuads: MutableMap<ResourceLocation, Map<EnumFacing, BakedQuad>> = mutableMapOf()
     val FACE_QUADS: Map<ResourceLocation, Map<EnumFacing, BakedQuad>> get() = _faceQuads
+    private lateinit var HULL_QUADS: Map<String, Map<EnumFacing, BakedQuad>>
+    private lateinit var HULL_TEXTURES: Map<String, TextureAtlasSprite>
+    private lateinit var inputModeQuads: Map<MachineIoMode, List<BakedQuad>?>
+    private lateinit var outputModeQuads: Map<MachineIoMode, List<BakedQuad>?>
+    private lateinit var filterQuads: List<BakedQuad>
 
     val faceBakery = FaceBakery()
     fun createQuad(side: EnumFacing, texture: TextureAtlasSprite): BakedQuad {
@@ -47,6 +62,7 @@ object ModelTextures {
 
     fun initialize(getter: java.util.function.Function<ResourceLocation, TextureAtlasSprite>) {
         if (isInitialized) return
+        isInitialized = true
         MISSING = getter.apply(ModelLoader.MODEL_MISSING)
 
         this.HULL_TEXTURES = ClayTiers.entries.associate {
@@ -67,7 +83,44 @@ object ModelTextures {
                 }
             }
         }
-        isInitialized = true
+        this.inputModeQuads = MachineIoMode.entries.associateWith { ioMode ->
+            val textureName = when (ioMode) {
+                NONE -> return@associateWith null
+                FIRST -> "import_1"
+                SECOND -> "import_2"
+                ALL -> "import"
+                CE -> "import_energy"
+                M_ALL -> "import_m0"
+                M_1 -> "import_m1"
+                M_2 -> "import_m2"
+                M_3 -> "import_m3"
+                M_4 -> "import_m4"
+                M_5 -> "import_m5"
+                M_6 -> "import_m6"
+            }
+            val atlasSprite = getter.apply(clayiumId("blocks/$textureName"))
+            EnumFacing.entries.map { side -> createQuad(side, atlasSprite) }
+        }
+        outputModeQuads = MachineIoMode.entries.associateWith { ioMode ->
+            val textureName = when (ioMode) {
+                NONE -> return@associateWith null
+                FIRST -> "export_1"
+                SECOND -> "export_2"
+                ALL -> "export"
+                CE -> return@associateWith null
+                M_ALL -> "export_m0"
+                M_1 -> "export_m1"
+                M_2 -> "export_m2"
+                M_3 -> "export_m3"
+                M_4 -> "export_m4"
+                M_5 -> "export_m5"
+                M_6 -> "export_m6"
+            }
+            val atlasSprite = getter.apply(clayiumId("blocks/$textureName"))
+            EnumFacing.entries.map { side -> createQuad(side, atlasSprite) }
+        }
+        val filterSprite = getter.apply(clayiumId("blocks/filter"))
+        filterQuads = EnumFacing.entries.map { createQuad(it, filterSprite) }
     }
 
     fun getHullTexture(tier: ITier): TextureAtlasSprite {
@@ -76,5 +129,17 @@ object ModelTextures {
 
     fun getHullQuads(tier: ITier): Map<EnumFacing, BakedQuad>? {
         return HULL_QUADS[tier.prefixTranslationKey]
+    }
+
+    fun getInputQuad(ioMode: MachineIoMode, facing: EnumFacing): BakedQuad? {
+        return inputModeQuads[ioMode]?.get(facing.index)
+    }
+
+    fun getOutputQuad(ioMode: MachineIoMode, facing: EnumFacing): BakedQuad? {
+        return outputModeQuads[ioMode]?.get(facing.index)
+    }
+
+    fun getFilterQuad(side: EnumFacing): BakedQuad {
+        return filterQuads[side.index]
     }
 }
