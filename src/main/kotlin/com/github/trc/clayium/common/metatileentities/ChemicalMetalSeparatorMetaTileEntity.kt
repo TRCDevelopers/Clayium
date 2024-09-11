ba1815e32bf575c2b4baa3c41ef8895e105b0cd5
bqc0n <89625049+bqc0n@users.noreply.github.com>
2024-09-11 15:43:39 +0900

    fix: Chemical Metal Separator GUI is broken

--- src/main/kotlin/com/github/trc/clayium/common/metatileentities/ChemicalMetalSeparatorMetaTileEntity.kt
+++ src/main/kotlin/com/github/trc/clayium/common/metatileentities/ChemicalMetalSeparatorMetaTileEntity.kt
@@ -1,5 +1,7 @@
 package com.github.trc.clayium.common.metatileentities
 
+import com.cleanroommc.modularui.api.drawable.IKey
+import com.cleanroommc.modularui.screen.ModularPanel
 import com.cleanroommc.modularui.utils.Alignment
 import com.cleanroommc.modularui.value.sync.GuiSyncManager
 import com.cleanroommc.modularui.value.sync.SyncHandlers
@@ -7,12 +9,17 @@ import com.cleanroommc.modularui.widget.ParentWidget
 import com.cleanroommc.modularui.widgets.ItemSlot
 import com.cleanroommc.modularui.widgets.SlotGroupWidget
 import com.cleanroommc.modularui.widgets.layout.Row
+import com.github.trc.clayium.api.GUI_DEFAULT_HEIGHT
+import com.github.trc.clayium.api.GUI_DEFAULT_WIDTH
 import com.github.trc.clayium.api.capability.impl.NotifiableItemStackHandler
 import com.github.trc.clayium.api.capability.impl.RecipeLogicEnergy
+import com.github.trc.clayium.api.gui.data.MetaTileEntityGuiData
 import com.github.trc.clayium.api.metatileentity.WorkableMetaTileEntity
 import com.github.trc.clayium.api.util.ITier
+import com.github.trc.clayium.api.util.asWidgetResizing
 import com.github.trc.clayium.api.util.clayiumId
 import com.github.trc.clayium.common.recipe.registry.CRecipes
+import net.minecraft.client.resources.I18n
 import net.minecraft.util.ResourceLocation
 
 class ChemicalMetalSeparatorMetaTileEntity(
@@ -25,7 +32,7 @@ class ChemicalMetalSeparatorMetaTileEntity(
     override val exportItems = NotifiableItemStackHandler(this, 116, this, isExport = true)
     override val workable = RecipeLogicEnergy(this, recipeRegistry, clayEnergyHolder)
 
-    override fun buildMainParentWidget(syncManager: GuiSyncManager): ParentWidget<*> {
+    override fun buildUI(data: MetaTileEntityGuiData, syncManager: GuiSyncManager): ModularPanel {
         val slotsAndProgressBar = Row()
             .widthRel(0.8f).height(18 * 4)
             .align(Alignment.Center)
@@ -43,12 +50,23 @@ class ChemicalMetalSeparatorMetaTileEntity(
                 .align(Alignment.CenterRight)
             )
 
-        return super.buildMainParentWidget(syncManager)
-            .child(slotsAndProgressBar)
-            .child(clayEnergyHolder.createSlotWidget()
-                .align(Alignment.BottomRight))
-            .child(clayEnergyHolder.createCeTextWidget(syncManager)
-                .bottom(12).left(0))
+        return ModularPanel.defaultPanel(translationKey, GUI_DEFAULT_WIDTH, GUI_DEFAULT_HEIGHT + 20)
+            .columnWithPlayerInv {
+                child(ParentWidget().widthRel(1f).expanded().marginBottom(2)
+                    .child(IKey.str(getStackForm().displayName).asWidget()
+                        .align(Alignment.TopLeft))
+                    .child(IKey.lang("container.inventory").asWidget().align(Alignment.BottomLeft))
+                    .child(IKey.dynamic {
+                        // if empty string, a bug occurs.
+                        if (overclock != 1.0) I18n.format("gui.clayium.overclock", overclock) else " "
+                    }.asWidgetResizing().alignment(Alignment.CenterRight).align(Alignment.BottomRight))
+                    .child(slotsAndProgressBar)
+                    .child(clayEnergyHolder.createSlotWidget()
+                        .align(Alignment.BottomRight))
+                    .child(clayEnergyHolder.createCeTextWidget(syncManager)
+                        .bottom(12).left(0))
+                )
+            }
     }
 
     override fun createMetaTileEntity() = ChemicalMetalSeparatorMetaTileEntity(metaTileEntityId, tier)
