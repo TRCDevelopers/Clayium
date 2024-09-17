d0335c1c63878d14239ba1a3f9ac4590a7cc5bd9
bqc0n <89625049+bqc0n@users.noreply.github.com>
2024-09-17 13:31:15 +0900

    feat: block model

--- src/main/kotlin/com/github/trc/clayium/common/metatileentities/DistributorMetaTileEntity.kt
+++ src/main/kotlin/com/github/trc/clayium/common/metatileentities/DistributorMetaTileEntity.kt
@@ -17,17 +17,27 @@ import com.github.trc.clayium.api.metatileentity.MetaTileEntity
 import com.github.trc.clayium.api.metatileentity.trait.AutoIoHandler
 import com.github.trc.clayium.api.util.ITier
 import com.github.trc.clayium.api.util.MachineIoMode
+import com.github.trc.clayium.api.util.clayiumId
 import com.github.trc.clayium.api.util.copyWithSize
 import com.github.trc.clayium.api.util.enumMapNotNull
 import com.github.trc.clayium.api.util.next
+import com.github.trc.clayium.client.model.ModelTextures
+import net.minecraft.block.state.IBlockState
+import net.minecraft.client.renderer.block.model.BakedQuad
+import net.minecraft.client.renderer.block.model.FaceBakery
+import net.minecraft.client.renderer.texture.TextureAtlasSprite
 import net.minecraft.nbt.NBTTagCompound
 import net.minecraft.util.EnumFacing
 import net.minecraft.util.ResourceLocation
 import net.minecraftforge.common.capabilities.Capability
+import net.minecraftforge.common.property.IExtendedBlockState
+import net.minecraftforge.fml.relauncher.Side
+import net.minecraftforge.fml.relauncher.SideOnly
 import net.minecraftforge.items.CapabilityItemHandler
 import net.minecraftforge.items.ItemHandlerHelper
 import net.minecraftforge.items.wrapper.CombinedInvWrapper
 import java.util.EnumMap
+import java.util.function.Function
 import kotlin.math.min
 
 class DistributorMetaTileEntity(
@@ -110,6 +120,19 @@ class DistributorMetaTileEntity(
             return null
         }
         return super.getCapability(capability, facing)
+    }
+
+    @SideOnly(Side.CLIENT)
+    override fun bakeQuads(getter: Function<ResourceLocation, TextureAtlasSprite>, faceBakery: FaceBakery) {
+        val sprite = getter.apply(clayiumId("blocks/distributor"))
+        distributorQuads = EnumFacing.entries.map { ModelTextures.createQuad(it, sprite) }
+    }
+
+    @SideOnly(Side.CLIENT)
+    override fun getQuads(quads: MutableList<BakedQuad>, state: IBlockState?, side: EnumFacing?, rand: Long) {
+        super.getQuads(quads, state, side, rand)
+        if (state == null || side == null || state !is IExtendedBlockState) return
+        quads.add(distributorQuads[side.index])
     }
 
     override fun createMetaTileEntity(): MetaTileEntity {
@@ -237,5 +260,9 @@ class DistributorMetaTileEntity(
             }
             if (remainingExport != amountPerAction) exportPtr++
         }
+    }
+
+    companion object {
+        private lateinit var distributorQuads: List<BakedQuad>
     }
 }
