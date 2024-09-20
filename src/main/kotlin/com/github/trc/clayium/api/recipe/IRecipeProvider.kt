183e90f9fb12da4db24d24bab48d0e5aa134f0d9
bqc0n <b4c0nchan@gmail.com>
2024-09-20 17:39:41 +0900

    feat: IRecipeProvider

---
+++ src/main/kotlin/com/github/trc/clayium/api/recipe/IRecipeProvider.kt
@@ -0,0 +1,16 @@
+package com.github.trc.clayium.api.recipe
+
+import com.github.trc.clayium.common.recipe.Recipe
+import net.minecraft.item.ItemStack
+
+/**
+ * Recipe search logic for [com.github.trc.clayium.api.capability.impl.AbstractRecipeLogic].
+ */
+interface IRecipeProvider {
+    /**
+     * null for disable JEI page for this logic.
+     */
+    val jeiCategory: String?
+
+    fun searchRecipe(machineTier: Int, inputs: List<ItemStack>): Recipe?
+}
