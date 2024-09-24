4a899920f4097d7fecf2f7f05a8a0b72bd209c91
bqc0n <b4c0nchan@gmail.com>
2024-09-24 14:08:00 +0900

    feat: MaterialAmount inline value class

--- src/main/kotlin/com/github/trc/clayium/api/CValues.kt
+++ src/main/kotlin/com/github/trc/clayium/api/CValues.kt
@@ -1,3 +1,7 @@
+/*
+ * some code is copied from `gregtech.api.GTValues`.
+ */
+
 package com.github.trc.clayium.api
 
 import net.minecraftforge.fml.common.FMLCommonHandler
@@ -11,6 +15,20 @@ const val GUI_DEFAULT_WIDTH = 176
 const val GUI_DEFAULT_HEIGHT = 166
 
 const val HARDNESS_UNBREAKABLE = -1.0f
+
+/**
+ * This is worth exactly one normal Item.
+ * This Constant can be divided by many commonly used Numbers such as
+ * 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 14, 15, 16, 18, 20, 21, 24, ... 64 or 81
+ * without losing precision and is for that reason used as Unit of Amount.
+ * But it is also small enough to be multiplied with larger Numbers.
+ *
+ * This is used to determine the amount of Material contained inside a prefixed Ore.
+ * For example, Nugget = `M / 9` as it contains out of 1/9 of an Ingot.
+ *
+ * Copied from `gregtech.api.GTValues`
+ */
+const val M: Long = 3628800L
 
 object CValues {
     const val MOD_ID = "clayium"
---
+++ src/main/kotlin/com/github/trc/clayium/api/unification/material/MaterialAmount.kt
@@ -0,0 +1,14 @@
+package com.github.trc.clayium.api.unification.material
+
+import com.github.trc.clayium.api.M
+
+@JvmInline
+value class MaterialAmount private constructor(val raw: Long) {
+
+    val dustAmount get() = raw / M
+
+    companion object {
+        fun of(dustAmount: Long) = MaterialAmount(M * dustAmount)
+        fun nugget(nuggetAmount: Long) = MaterialAmount((M / 9) * nuggetAmount)
+    }
+}
