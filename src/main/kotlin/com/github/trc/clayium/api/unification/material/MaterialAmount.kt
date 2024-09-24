41d4e2ba39bc29a91b52ec3fb9cb1ddb6d6ff281
bqc0n <89625049+bqc0n@users.noreply.github.com>
2024-09-24 21:48:44 +0900

    feat: more amounts

--- src/main/kotlin/com/github/trc/clayium/api/unification/material/MaterialAmount.kt
+++ src/main/kotlin/com/github/trc/clayium/api/unification/material/MaterialAmount.kt
@@ -8,6 +8,7 @@ value class MaterialAmount private constructor(val raw: Long) {
     val dustAmount get() = raw / M
 
     companion object {
+        val NONE = MaterialAmount(-1)
         fun of(dustAmount: Long) = MaterialAmount(M * dustAmount)
         fun nugget(nuggetAmount: Long) = MaterialAmount((M / 9) * nuggetAmount)
     }
