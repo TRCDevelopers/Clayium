add524d5f6bd98956085536635be3abbffe4ad0a
bqc0n <b4c0nchan@gmail.com>
2024-10-02 20:23:57 +0900

    refactor: namespaced mte registry (wip)

---
+++ src/main/kotlin/com/github/trc/clayium/api/metatileentity/registry/CMteManager.kt
@@ -0,0 +1,39 @@
+package com.github.trc.clayium.api.metatileentity.registry
+
+import it.unimi.dsi.fastutil.ints.Int2ObjectOpenHashMap
+import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap
+import org.jetbrains.annotations.UnmodifiableView
+
+object CMteManager {
+    private var currentNetworkId = 0
+    private val modId2Registry = Object2ObjectOpenHashMap<String, CMteRegistry>()
+    private val networkIdMap = Int2ObjectOpenHashMap<CMteRegistry>()
+
+    /**
+     * Gets the registry for the given modId.
+     * @return The registry, or null if it doesn't exist
+     */
+    fun getRegistry(modId: String): CMteRegistry? {
+        return modId2Registry[modId]
+    }
+
+    /**
+     * Creates a new MetaTileEntity registry for the given modId.
+     * @throws IllegalArgumentException If a registry for the modId already exists
+     * @return The created registry
+     */
+    fun createRegistry(modId: String): CMteRegistry {
+        val existing = modId2Registry[modId]
+        if (existing != null) {
+            throw IllegalArgumentException("Registry for mod $modId already exists")
+        }
+        val registry = CMteRegistry(modId, currentNetworkId++)
+        modId2Registry[modId] = registry
+        networkIdMap[registry.networkId] = registry
+        return registry
+    }
+
+    fun allRegistries(): @UnmodifiableView Collection<CMteRegistry> {
+        return modId2Registry.values
+    }
+}
---
+++ src/main/kotlin/com/github/trc/clayium/api/metatileentity/registry/CMteRegistry.kt
@@ -0,0 +1,30 @@
+package com.github.trc.clayium.api.metatileentity.registry
+
+import com.github.trc.clayium.api.MOD_ID
+import com.github.trc.clayium.api.block.BlockMachine
+import com.github.trc.clayium.api.metatileentity.MetaTileEntity
+import com.github.trc.clayium.api.util.registry.CRegistry
+import net.minecraft.util.ResourceLocation
+import net.minecraftforge.fml.common.Loader
+import net.minecraftforge.fml.common.LoaderState
+
+class CMteRegistry(
+    val modId: String,
+    val networkId: Int,
+) : CRegistry<ResourceLocation, MetaTileEntity>(Short.MAX_VALUE.toInt()) {
+
+    val blockMachine by lazy {
+        if (!Loader.instance().hasReachedState(LoaderState.PREINITIALIZATION)) throw IllegalStateException("Too early to access blockMachine")
+        BlockMachine().apply {
+            // todo: datafix?
+            registryName = if (modId == MOD_ID) ResourceLocation(modId, "machine") else ResourceLocation(modId, "clayium_machine")
+        }
+    }
+
+    override fun register(id: Int, key: ResourceLocation, value: MetaTileEntity) {
+        if (key.namespace != modId) {
+            throw IllegalArgumentException("Cannot register a MetaTileEntity to another mod's registry")
+        }
+        super.register(id, key, value)
+    }
+}
