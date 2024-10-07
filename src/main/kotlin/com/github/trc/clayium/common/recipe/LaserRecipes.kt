50e914d4dbd22e736b01532c406acd41721f732f
soramame0256 <62879324+soramame0256@users.noreply.github.com>
2024-10-07 10:58:10 +0900

    fix: removed val LASER from LaserRecipes and make refer CRecipes' one

--- src/main/kotlin/com/github/trc/clayium/common/recipe/LaserRecipes.kt
+++ src/main/kotlin/com/github/trc/clayium/common/recipe/LaserRecipes.kt
@@ -1,10 +1,9 @@
 package com.github.trc.clayium.common.recipe
 
-import com.github.trc.clayium.common.recipe.registry.LaserRecipeRegistry
+import com.github.trc.clayium.common.recipe.registry.CRecipes.LASER
 import net.minecraft.block.state.IBlockState
 
 object LaserRecipes {
-    val LASER = LaserRecipeRegistry()
 
     fun getLaserRecipe(input: IBlockState, energy: Double): LaserRecipe? {
         return LASER.getRecipe(input, energy)
