55594da19ca16d02aa0390a5eefbc935203ba3fe
bqc0n <b4c0nchan@gmail.com>
2025-02-12 23:08:36 +0900

    fix: no /dev/tty in github actions

--- .github/actions/run_server_autostop.sh
+++ .github/actions/run_server_autostop.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-stdbuf -oL bash -c "./gradlew runServer" | tee /dev/tty | while read line; do
+stdbuf -oL bash -c "./gradlew runServer" | while read line; do
+  echo $line
   if [ "$(echo $line | grep -c "Done")" -gt 0 ]; then
     echo "Server has started"
     pkill -P $$
