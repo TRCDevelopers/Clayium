4ed8c063d8d7f299585fa46e9d1355a57055f5e1
bqc0n <b4c0nchan@gmail.com>
2025-02-13 10:28:46 +0900

    or true

--- .github/actions/run_server_autostop.sh
+++ .github/actions/run_server_autostop.sh
@@ -4,7 +4,7 @@ stdbuf -oL bash -c "./gradlew runServer" | while read line; do
   echo $line
   if [ "$(echo $line | grep -c "Done")" -gt 0 ]; then
     echo "Server has started"
-    pkill -P $$
+    pkill -P $$ # kill all child processes
     exit 0
   fi
 done
