fd4a912d8d8fb6e2be1ee96970ab7d687c440218
bqc0n <89625049+bqc0n@users.noreply.github.com>
2025-03-21 22:21:16 +0900

    fix: OreDictionary Input

--- src/main/kotlin/com/github/trc/clayium/common/recipe/ingredient/CItemRecipeInput.kt
+++ src/main/kotlin/com/github/trc/clayium/common/recipe/ingredient/CItemRecipeInput.kt
@@ -2,6 +2,7 @@ package com.github.trc.clayium.common.recipe.ingredient
 
 import com.github.trc.clayium.api.unification.stack.ItemAndMeta
 import net.minecraft.item.ItemStack
+import net.minecraftforge.oredict.OreDictionary
 
 class CItemRecipeInput(
     override val stacks: List<ItemStack>,
@@ -18,15 +19,14 @@ class CItemRecipeInput(
 
     override fun testItemStackAndAmount(stack: ItemStack): Boolean {
         return stacks.any {
-            ItemStack.areItemsEqual(it, stack)
-                    && (!stack.hasSubtypes || stack.metadata == it.metadata || stack.metadata == 32767)
+            OreDictionary.itemMatches(it, stack, false)
                     && stack.count >= amount
         }
     }
 
     override fun testIgnoringAmount(item: ItemAndMeta): Boolean {
         return stacks.any {
-            item.item == it.item && (item.meta == it.metadata || it.metadata == 32767)
+            OreDictionary.itemMatches(it, item.asStack(), false)
         }
     }
 
