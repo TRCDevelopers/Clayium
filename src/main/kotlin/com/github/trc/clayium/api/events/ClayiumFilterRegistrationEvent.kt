020783b099e2490f534164d555edc9f97adf0009
bqc0n <89625049+bqc0n@users.noreply.github.com>
2025-06-30 21:02:50 +0900

    feat: remove filter registry

--- src/main/kotlin/com/github/trc/clayium/api/events/ClayiumFilterRegistrationEvent.kt
+++
@@ -1,8 +0,0 @@
-package com.github.trc.clayium.api.events
-
-import com.github.trc.clayium.api.item.filter.ItemFilterRegistry
-import net.minecraftforge.fml.common.eventhandler.Event
-
-class ClayiumFilterRegistrationEvent(
-    val registry: ItemFilterRegistry,
-) : Event()
