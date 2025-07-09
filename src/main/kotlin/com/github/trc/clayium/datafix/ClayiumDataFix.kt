b6540ae9b1c6c433821b43c8fe57b235f68fa35c
bqc0n <89625049+bqc0n@users.noreply.github.com>
2025-07-09 21:43:32 +0900

    feat: datafix

--- src/main/kotlin/com/github/trc/clayium/datafix/ClayiumDataFix.kt
+++ src/main/kotlin/com/github/trc/clayium/datafix/ClayiumDataFix.kt
@@ -1,6 +1,7 @@
 package com.github.trc.clayium.datafix
 
 import com.github.trc.clayium.api.MOD_ID
+import com.github.trc.clayium.datafix.fixable.ClayMarkerHandlerMigrator
 import com.github.trc.clayium.datafix.fixable.MteHolderMigrator
 import net.minecraft.util.datafix.FixTypes
 import net.minecraftforge.fml.common.FMLCommonHandler
@@ -10,5 +11,6 @@ object ClayiumDataFix {
         val globalFixer = FMLCommonHandler.instance().dataFixer
         val modFixs = globalFixer.init(MOD_ID, ClayiumDataVersion.currentVersion.ordinal)
         modFixs.registerFix(FixTypes.BLOCK_ENTITY, MteHolderMigrator())
+        modFixs.registerFix(FixTypes.BLOCK_ENTITY, ClayMarkerHandlerMigrator())
     }
 }
--- src/main/kotlin/com/github/trc/clayium/datafix/ClayiumDataVersion.kt
+++ src/main/kotlin/com/github/trc/clayium/datafix/ClayiumDataVersion.kt
@@ -3,6 +3,10 @@ package com.github.trc.clayium.datafix
 enum class ClayiumDataVersion {
     V0,
     V1_MORE_FILTERS,
+    /**
+     * Ranged machines now save its range using 2 [net.minecraft.util.math.BlockPos]es instead of [codechicken.lib.vec.Cuboid6].
+     */
+    V2_RANGED_MACHINES_CHANGE_FORMAT,
     ;
 
     companion object {
