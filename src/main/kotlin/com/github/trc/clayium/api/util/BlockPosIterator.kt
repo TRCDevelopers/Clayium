d77a8f63d8fd5e6952c865756ab5a785c53e2884
bqc0n <me@bqc0n.com>
2025-07-10 10:48:07 +0900

    fix: calculate min/max of given BlockPoses

--- src/main/kotlin/com/github/trc/clayium/api/util/BlockPosIterator.kt
+++ src/main/kotlin/com/github/trc/clayium/api/util/BlockPosIterator.kt
@@ -1,57 +1,52 @@
 package com.github.trc.clayium.api.util
 
 import net.minecraft.util.math.BlockPos
+import kotlin.math.max
+import kotlin.math.min
 
 /**
  * Iterates a square constructed from given block poses.
  * The iteration order is x -> z -> y.
  * x, z are iterated in ascending order, while y is iterated in descending order.
  */
-class BlockPosIterator(
-    first: BlockPos,
-    last: BlockPos,
-) : Iterator<BlockPos> {
+class BlockPosIterator(first: BlockPos, last: BlockPos) : AbstractIterator<BlockPos>() {
 
     constructor(pair: Pair<BlockPos, BlockPos>) : this(pair.first, pair.second)
 
-    private val xRange = first.x..last.x
-    private val zRange = first.z..last.z
-    private val yRange = (first.y..last.y).reversed()
+    private val min = BlockPos(min(first.x, last.x), min(first.y, last.y), min(first.z, last.z))
+    private val max = BlockPos(max(first.x, last.x), max(first.y, last.y), max(first.z, last.z))
 
-    private var xIter = xRange.iterator()
-    private var zIter = zRange.iterator()
-    private var yIter = yRange.iterator()
+    private var x = min.x
+    private var y = max.y
+    private var z = min.z
 
-    override fun hasNext(): Boolean {
-        return xIter.hasNext() || yIter.hasNext() || zIter.hasNext()
-    }
+    private var init = true
 
-    var lastY = yIter.next()
-    var lastZ = zIter.next()
-
-    override fun next(): BlockPos {
-        return if (xIter.hasNext()) {
-            BlockPos(xIter.next(), lastY, lastZ)
-        } else if (zIter.hasNext()) {
-            xIter = xRange.iterator()
-            lastZ = zIter.next()
-            BlockPos(xIter.next(), lastY, lastZ)
-        } else if (yIter.hasNext()) {
-            xIter = xRange.iterator()
-            zIter = zRange.iterator()
-            lastY = yIter.next()
-            lastZ = zIter.next()
-            BlockPos(xIter.next(), lastY, lastZ)
-        } else {
-            throw NoSuchElementException("No more elements in BlockPosIterator")
+    override fun computeNext() {
+        if (this.init) {
+            this.init = false
+            return this.setNext(BlockPos(this.x, this.y, this.z))
         }
+
+        if (this.x < max.x) {
+            this.x++
+        } else if (this.z < max.z) {
+            this.x = min.x
+            this.z++
+        } else if (this.y > min.y) {
+            this.x = min.x
+            this.z = min.z
+            this.y--
+        } else {
+            return done()
+        }
+        setNext(BlockPos(this.x, this.y, this.z))
     }
 
     fun restart() {
-        xIter = xRange.iterator()
-        zIter = zRange.iterator()
-        yIter = yRange.iterator()
-        lastY = yIter.next()
-        lastZ = zIter.next()
+        this.x = min.x
+        this.y = max.y
+        this.z = min.z
+        this.computeNext()
     }
 }
