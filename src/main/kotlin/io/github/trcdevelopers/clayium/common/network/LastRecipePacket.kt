27dffa47ab7734f7ae2c68fb3aa5a883fc3aebe2
bqc0n <89625049+bqc0n@users.noreply.github.com>
2025-08-10 22:33:14 +0900

    docs: doc comment

--- src/main/kotlin/io/github/trcdevelopers/clayium/common/network/LastRecipePacket.kt
+++ src/main/kotlin/io/github/trcdevelopers/clayium/common/network/LastRecipePacket.kt
@@ -11,6 +11,7 @@ import net.minecraftforge.fml.common.network.simpleimpl.IMessage
 import net.minecraftforge.fml.common.network.simpleimpl.IMessageHandler
 import net.minecraftforge.fml.common.network.simpleimpl.MessageContext
 
+// Packet Class must have a no-arg constructor
 class LastRecipePacket @JvmOverloads constructor(
     var recipe: IRecipe? = null,
 ) : IMessage {
