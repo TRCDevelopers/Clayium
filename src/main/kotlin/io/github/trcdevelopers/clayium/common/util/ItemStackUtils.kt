218c880a62cf99e918346b1c4507b8db1faa9a9a
bqc0n <89625049+bqc0n@users.noreply.github.com>
2025-08-12 22:42:47 +0900

    rm dead code

--- src/main/kotlin/io/github/trcdevelopers/clayium/common/util/ItemStackUtils.kt
+++ src/main/kotlin/io/github/trcdevelopers/clayium/common/util/ItemStackUtils.kt
@@ -5,9 +5,3 @@ import net.minecraft.item.ItemStack
 fun ItemStack.isIdenticalTo(other: ItemStack): Boolean {
     return this.isItemEqual(other) && ItemStack.areItemStackTagsEqual(this, other)
 }
-
-object ItemStackUtils {
-    fun isItemStackIdentical(stack1: ItemStack, stack2: ItemStack): Boolean {
-        return stack1.isItemEqual(stack2) && ItemStack.areItemStackTagsEqual(stack1, stack2)
-    }
-}
