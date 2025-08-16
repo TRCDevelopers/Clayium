01989b581fa1860fe8053b54c0d90863c274a299
bqc0n <89625049+bqc0n@users.noreply.github.com>
2025-08-17 00:27:36 +0900

    kts

---
+++ build.gradle.kts
@@ -0,0 +1,275 @@
+import org.jetbrains.gradle.ext.Gradle
+import org.jetbrains.gradle.ext.compiler
+import org.jetbrains.gradle.ext.runConfigurations
+import org.jetbrains.gradle.ext.settings
+
+plugins {
+    id("java")
+    id("java-library")
+    kotlin("jvm") version "2.0.10"
+    id("maven-publish")
+    id("org.jetbrains.gradle.plugin.idea-ext") version "1.2"
+    id("eclipse")
+    id("com.gtnewhorizons.retrofuturagradle") version "1.4.6"
+}
+
+val forgelinContinuousVersion: String by project
+val mixinBooterVersion: String by project
+
+val modName: String by project
+val modId: String by project
+val modGroup: String by project
+val modVersion: String by project
+val modArchivesBaseName: String by project
+
+val accessTransformersFile: String by project
+
+val gradleTokenClassName: String by project
+
+val developmentEnvironmentUserName: String by project
+
+val coreMod: String by project
+val coreModClass: String by project
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(8))
+        // Azul covers the most platforms for Java 8 toolchains, crucially including MacOS arm64
+        vendor.set(JvmVendorSpec.AZUL)
+    }
+    // Generate sources and javadocs jars when building and publishing
+    withSourcesJar()
+    withJavadocJar()
+}
+
+tasks.withType<JavaCompile>().configureEach {
+    options.encoding = "UTF-8"
+}
+
+configurations {
+    val embed = create("embed")
+    implementation.configure {
+        extendsFrom(embed)
+    }
+
+//    create("runtimeOnlyNonPublishable") {
+//        description = "Runtime only dependencies that are not published alongside the jar"
+//        isCanBeConsumed = false
+//        isCanBeResolved = false
+//    }
+//    create("devOnlyNonPublishable") {
+//        description = "Runtime and compiletime dependencies that are not published alongside the jar (compileOnly + runtimeOnlyNonPublishable)"
+//        isCanBeConsumed = false
+//        isCanBeResolved = false
+//    }
+}
+
+minecraft {
+    mcVersion.set("1.12.2")
+
+    // MCP Mappings
+    mcpMappingChannel.set("stable")
+    mcpMappingVersion.set("39")
+
+    // Set username here, the UUID will be looked up automatically
+    username.set(developmentEnvironmentUserName)
+
+    // Add any additional tweaker classes here
+    // extraTweakClasses.add("org.spongepowered.asm.launch.MixinTweaker")
+
+    // Add various JVM arguments here for runtime
+    val args = mutableListOf("-ea:${group}")
+    if (coreMod.toBoolean()) {
+        args += "-Dfml.coreMods.load=$coreModClass"
+    }
+    if (coreMod.toBoolean()) {
+        args += "-Dmixin.hotSwap=true"
+        args += "-Dmixin.checks.interfaces=true"
+        args += "-Dmixin.debug.export=true"
+    }
+    extraRunJvmArguments.addAll(args)
+
+    // Include and use dependencies' Access Transformer files
+    useDependencyAccessTransformers.set(true)
+
+    // Add any properties you want to swap out for a dynamic value at build time here
+    // Any properties here will be added to a class at build time, the name can be configured below
+    // Example:
+    injectedTags.put("VERSION", project.version)
+    injectedTags.put("MOD_ID", modId)
+    injectedTags.put("MOD_NAME", modName)
+}
+
+// Generate a group.archives_base_name.Tags class
+tasks.injectTags.configure {
+    // Change Tags class' name here:
+    outputClassName.set(gradleTokenClassName)
+}
+
+repositories {
+    maven {
+        name = "BlameJared Maven"
+        url = uri("https://maven.blamejared.com")
+    }
+    maven {
+        name = "CleanroomMC Maven"
+        url = uri("https://maven.cleanroommc.com")
+    }
+    maven {
+        name = "GTNH Maven"
+        url = uri("https://nexus.gtnewhorizons.com/repository/public/")
+    }
+    maven {
+        name = "GTCEu Maven"
+        url = uri("https://maven.gtceu.com")
+    }
+    maven {
+        name = "SpongePowered Maven"
+        url = uri("https://repo.spongepowered.org/maven")
+    }
+    maven {
+        name = "CurseMaven"
+        url = uri("https://cursemaven.com")
+        content {
+            includeGroup("curse.maven")
+        }
+    }
+    maven {
+        name = "Modrinth"
+        url = uri("https://api.modrinth.com/maven")
+        content {
+            includeGroup("maven.modrinth")
+        }
+    }
+
+
+    maven {
+        name = "thiakil"
+        url = uri("http://maven.thiakil.com")
+        isAllowInsecureProtocol = true
+    }
+
+    mavenLocal() // Must be last for caching to work
+}
+
+dependencies {
+    runtimeOnly("io.github.chaosunity.forgelin:Forgelin-Continuous:${forgelinContinuousVersion}") {
+        exclude("net.minecraftforge")
+    }
+    /* mixin */
+    val mixin = modUtils.enableMixins("zone.rong:mixinbooter:$mixinBooterVersion", "mixins.$modId.refmap.json") as String
+    api(mixin) {
+        isTransitive = false
+    }
+    annotationProcessor("org.ow2.asm:asm-debug-all:5.2")
+    annotationProcessor("com.google.guava:guava:32.1.2-jre")
+    annotationProcessor("com.google.code.gson:gson:2.8.9")
+    annotationProcessor(mixin) {
+        isTransitive = false
+    }
+
+    api("com.cleanroommc:modularui:2.5.0-rc5")
+    api("codechicken:codechickenlib:3.2.3.358")
+    api("com.cleanroommc:groovyscript:1.2.5") { isTransitive = false }
+
+    compileOnly("gregtech:gregtech:2.8.10-beta")
+    compileOnly(rfg.deobf("com.azanor.baubles:Baubles:1.12-1.5.2"))
+
+    testImplementation("io.kotest:kotest-runner-junit5:5.9.1")
+    testImplementation("io.mockk:mockk:1.14.5")
+
+    implementation("mezz.jei:jei_1.12.2:4.16.1.302")
+    implementation(rfg.deobf("curse.maven:top-245211:2667280")) // TOP 1.4.28
+}
+
+if (accessTransformersFile.isNotBlank()) {
+    val fileLocation = file("$projectDir/src/main/resources/$accessTransformersFile")
+    if (fileLocation.exists()) {
+        tasks.deobfuscateMergedJarToSrg.configure {
+            accessTransformerFiles.from(fileLocation)
+        }
+        tasks.srgifyBinpatchedJar.configure {
+            accessTransformerFiles.from(fileLocation)
+        }
+    } else {
+        throw GradleException("Access transformers file '$accessTransformersFile' does not exist at expected location: $fileLocation")
+    }
+}
+
+tasks.withType<ProcessResources> {
+    // This will ensure that this task is redone when the versions change
+    inputs.property("version", modVersion)
+    inputs.property("mcversion", minecraft.mcVersion)
+
+    // Replace various properties in mcmod.info and pack.mcmeta if applicable
+    filesMatching(arrayListOf("mcmod.info", "pack.mcmeta")) {
+        expand(
+            "version" to modVersion,
+            "mcversion" to minecraft.mcVersion
+        )
+    }
+
+    if (accessTransformersFile.isNotBlank()) {
+        rename("(.+_at.cfg)", "META-INF/$1") // Make sure Access Transformer files are in META-INF folder
+    }
+}
+
+tasks.withType<Jar> {
+    manifest {
+        val attributeMap = mutableMapOf<String, String>()
+//        if (use_coremod.toBoolean()) {
+//            attributeMap["FMLCorePlugin"] = coremod_plugin_class_name
+//            if (include_mod.toBoolean()) {
+//                attributeMap["FMLCorePluginContainsFMLMod"] = true.toString()
+//                attributeMap["ForceLoadAsMod"] = (project.gradle.startParameter.taskNames[0] == "build").toString()
+//            }
+//        }
+        if (accessTransformersFile.isNotBlank()) {
+            attributeMap["FMLAT"] = accessTransformersFile
+        }
+        attributes(attributeMap)
+    }
+    // Add all embedded dependencies into the jar
+    from(provider {
+        configurations.getByName("embed").map {
+            if (it.isDirectory()) it else zipTree(it)
+        }
+    })
+}
+
+
+idea {
+    module {
+        inheritOutputDirs = true
+    }
+    project {
+        settings {
+            runConfigurations {
+                add(Gradle("1. Run Client").apply {
+                    setProperty("taskNames", listOf("runClient"))
+                })
+                add(Gradle("2. Run Server").apply {
+                    setProperty("taskNames", listOf("runServer"))
+                })
+                add(Gradle("3. Run Obfuscated Client").apply {
+                    setProperty("taskNames", listOf("runObfClient"))
+                })
+                add(Gradle("4. Run Obfuscated Server").apply {
+                    setProperty("taskNames", listOf("runObfServer"))
+                })
+            }
+            compiler.javac {
+                afterEvaluate {
+                    javacAdditionalOptions = "-encoding utf8"
+                    moduleJavacAdditionalOptions = mutableMapOf(
+                        (project.name + ".main") to tasks.compileJava.get().options.compilerArgs.joinToString(" ") { "\"$it\"" }
+                    )
+                }
+            }
+        }
+    }
+}
+
+tasks.named("processIdeaSettings").configure {
+    dependsOn("injectTags")
+}
