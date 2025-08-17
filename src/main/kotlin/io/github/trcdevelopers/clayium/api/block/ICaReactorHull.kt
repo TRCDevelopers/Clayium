bcf00263ec4eae9836384e241b283658575df1d4
bqc0n <89625049+bqc0n@users.noreply.github.com>
2025-08-17 16:06:39 +0900

    refactor: getCaRank(stack) is not needed

--- src/main/kotlin/io/github/trcdevelopers/clayium/api/block/ICaReactorHull.kt
+++ src/main/kotlin/io/github/trcdevelopers/clayium/api/block/ICaReactorHull.kt
@@ -1,10 +1,12 @@
 package io.github.trcdevelopers.clayium.api.block
 
-import net.minecraft.item.ItemStack
 import net.minecraft.util.math.BlockPos
 import net.minecraft.world.IBlockAccess
 
+/**
+ * An interface for blocks.
+ * Implementing this interface allows the block to be recognized as a reactor hull.
+ */
 interface ICaReactorHull {
-    fun getCaRank(stack: ItemStack): Int
     fun getCaRank(world: IBlockAccess, pos: BlockPos): Int
 }
--- src/main/kotlin/io/github/trcdevelopers/clayium/common/blocks/BlockCaReactorHull.kt
+++ src/main/kotlin/io/github/trcdevelopers/clayium/common/blocks/BlockCaReactorHull.kt
@@ -40,7 +40,6 @@ class BlockCaReactorHull : Block(Material.IRON), ICaReactorHull, ITieredBlock {
         }
     }
 
-    override fun getCaRank(stack: ItemStack) = stack.metadata + 1
     override fun getCaRank(world: IBlockAccess, pos: BlockPos) = world.getBlockState(pos).getValue(META) + 1
 
     override fun createBlockState() = BlockStateContainer(this, META)
