e717b39aa9c41cbd508d6133ee5c518639cd5e93
bqc0n <89625049+bqc0n@users.noreply.github.com>
2025-10-04 20:11:38 +0900

    fix conflicts

--- src/main/kotlin/io/github/trcdevelopers/clayium/common/gui/ClayGuiTextures.kt
+++ src/main/kotlin/io/github/trcdevelopers/clayium/common/gui/ClayGuiTextures.kt
@@ -8,56 +8,56 @@ import io.github.trcdevelopers.clayium.api.util.clayiumId
 object ClayGuiTextures {
 
     val LARGE_SLOT: UITexture = slotTexture()
-        .uv(0, 32, 26, 26)
+        .xy(0, 32, 26, 26)
         .canApplyTheme()
         .build()
 
     val CLAY_SLOT: UITexture = slotTexture()
-        .uv(96, 0, 18, 18)
+        .xy(96, 0, 18, 18)
         .build()
 
     val FILTER_SLOT: UITexture = slotTexture()
-        .uv(96, 32, 18, 18)
+        .xy(96, 32, 18, 18)
         .build()
 
     // memo: gui/slot slot interval is 14 pixels
     val IMPORT_1_SLOT: UITexture = slotTexture()
-        .uv(32, 0, 18, 18)
+        .xy(32, 0, 18, 18)
         .build()
     val IMPORT_2_SLOT: UITexture = slotTexture()
-        .uv(32, 32, 18, 18)
+        .xy(32, 32, 18, 18)
         .build()
 
     val EXPORT_1_SLOT: UITexture = slotTexture()
-        .uv(64, 0, 18, 18)
+        .xy(64, 0, 18, 18)
         .build()
     val EXPORT_2_SLOT: UITexture = slotTexture()
-        .uv(64, 32, 18, 18)
+        .xy(64, 32, 18, 18)
         .build()
 
     /* Multi-trac buffer Slots */
-    val SLOT_M1: UITexture = slotTexture().uv(32, 96, 18, 18).build()
-    val SLOT_M2: UITexture = slotTexture().uv(64, 96, 18, 18).build()
-    val SLOT_M3: UITexture = slotTexture().uv(96, 96, 18, 18).build()
-    val SLOT_M4: UITexture = slotTexture().uv(128, 96, 18, 18).build()
-    val SLOT_M5: UITexture = slotTexture().uv(160, 96, 18, 18).build()
-    val SLOT_M6: UITexture = slotTexture().uv(192, 96, 18, 18).build()
+    val SLOT_M1: UITexture = slotTexture().xy(32, 96, 18, 18).build()
+    val SLOT_M2: UITexture = slotTexture().xy(64, 96, 18, 18).build()
+    val SLOT_M3: UITexture = slotTexture().xy(96, 96, 18, 18).build()
+    val SLOT_M4: UITexture = slotTexture().xy(128, 96, 18, 18).build()
+    val SLOT_M5: UITexture = slotTexture().xy(160, 96, 18, 18).build()
+    val SLOT_M6: UITexture = slotTexture().xy(192, 96, 18, 18).build()
 
     val M_TRACK_SLOTS = arrayOf(SLOT_M1, SLOT_M2, SLOT_M3, SLOT_M4, SLOT_M5, SLOT_M6)
 
-    val FILTER_SLOT_M1: UITexture = slotTexture().uv(32, 128, 18, 18).build()
-    val FILTER_SLOT_M2: UITexture = slotTexture().uv(64, 128, 18, 18).build()
-    val FILTER_SLOT_M3: UITexture = slotTexture().uv(96, 128, 18, 18).build()
-    val FILTER_SLOT_M4: UITexture = slotTexture().uv(128, 128, 18, 18).build()
-    val FILTER_SLOT_M5: UITexture = slotTexture().uv(160, 128, 18, 18).build()
-    val FILTER_SLOT_M6: UITexture = slotTexture().uv(192, 128, 18, 18).build()
+    val FILTER_SLOT_M1: UITexture = slotTexture().xy(32, 128, 18, 18).build()
+    val FILTER_SLOT_M2: UITexture = slotTexture().xy(64, 128, 18, 18).build()
+    val FILTER_SLOT_M3: UITexture = slotTexture().xy(96, 128, 18, 18).build()
+    val FILTER_SLOT_M4: UITexture = slotTexture().xy(128, 128, 18, 18).build()
+    val FILTER_SLOT_M5: UITexture = slotTexture().xy(160, 128, 18, 18).build()
+    val FILTER_SLOT_M6: UITexture = slotTexture().xy(192, 128, 18, 18).build()
 
     val M_TRACK_FILTER_SLOTS = arrayOf(FILTER_SLOT_M1, FILTER_SLOT_M2, FILTER_SLOT_M3, FILTER_SLOT_M4, FILTER_SLOT_M5, FILTER_SLOT_M6)
 
     val PROGRESS_BAR: UITexture = UITexture.builder()
         .location(clayiumId("gui/progress_bar"))
         .imageSize(256, 256)
-        .uv(1, 0, 22, 34)
+        .xy(1, 0, 22, 34)
         .canApplyTheme()
         .build()
 
@@ -65,7 +65,7 @@ object ClayGuiTextures {
     val BUTTON_PRESSED: UITexture = UITexture.builder()
             .location(Mods.ModularUI.modId, "gui/widgets/mc_button")
             .imageSize(16, 32)
-            .uv(0, 16, 16, 16)
+            .xy(0, 16, 16, 16)
             .name("mc_button_hovered")
             .build()
 
@@ -133,7 +133,7 @@ object ClayGuiTextures {
         val PROGRESS_BAR_EMPTY: UITexture = UITexture.builder()
             .location(clayiumId("gui/clay_work_table_icons"))
             .imageSize(256, 256)
-            .uv(0, 0, 80, 32)
+            .xy(0, 0, 80, 32)
             .canApplyTheme()
             .build()
     }
@@ -145,6 +145,6 @@ object ClayGuiTextures {
     private fun button(u: Int, v: Int): UITexture = UITexture.builder()
         .location(MOD_ID, "gui/button")
         .imageSize(256, 256)
-        .uv(u, v, 16, 16)
+        .xy(u, v, 16, 16)
         .build()
 }
