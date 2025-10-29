9a1cafd9bef8c1ace46593f9f7625de9647ae72d
bqc0n <me@bqc0n.com>
2025-10-29 22:23:41 +0900

    format

--- src/main/java/io/github/trcdevelopers/clayium/mixins/patch/MixinNarrator.java
+++ src/main/java/io/github/trcdevelopers/clayium/mixins/patch/MixinNarrator.java
@@ -4,6 +4,7 @@ import com.mojang.text2speech.Narrator;
 import com.mojang.text2speech.NarratorDummy;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
+
 @Mixin(value = Narrator.class, remap = false)
 public interface MixinNarrator {
 
