f438b63c22df92ede7a7d43b2323c02bda39f233
bqc0n <me@bqc0n.com>
2025-10-30 11:21:28 +0900

    fix: @reason tag is unknown

--- src/main/java/io/github/trcdevelopers/clayium/mixins/patch/MixinNarrator.java
+++ src/main/java/io/github/trcdevelopers/clayium/mixins/patch/MixinNarrator.java
@@ -10,10 +10,12 @@ public interface MixinNarrator {
 
     /**
      * @author bqc0n
-     * @reason NarratorOSX {@link com.mojang.text2speech.NarratorOSX} causes UnsatisfiedLinkError on Apple Silicon Macs
+     * <p>
+     * NarratorOSX {@link com.mojang.text2speech.NarratorOSX} causes UnsatisfiedLinkError on Apple Silicon Macs
      * (Maybe native libs are not available in arm64 Java 8 JDKs?).
      * This mixin should be enabled only on deobf environment.
      */
+    @SuppressWarnings("OverwriteAuthorRequired") // reason is an unknown tag
     @Overwrite(remap = false)
     static Narrator getNarrator() {
         return new NarratorDummy();
