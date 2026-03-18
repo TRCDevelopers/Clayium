49b5239c3fc63a97c14b24452428e1e7a06c0418
bqc0n <89625049+bqc0n@users.noreply.github.com>
2026-03-18 15:32:27 +0900

    backword compatibility

---
+++ src/main/kotlin/io/github/trcdevelopers/clayium/api/gui/sync/ClayLaserSyncValue.kt
@@ -0,0 +1,35 @@
+package io.github.trcdevelopers.clayium.api.gui.sync
+
+import com.cleanroommc.modularui.value.sync.GenericSyncValue
+import io.github.trcdevelopers.clayium.api.gui.sync.codec.ClayLaserByteBufAdapter
+import io.github.trcdevelopers.clayium.api.laser.ClayLaser
+import org.jetbrains.annotations.ApiStatus
+import java.util.function.Consumer
+import java.util.function.Supplier
+
+@Suppress("DEPRECATION")
+@Deprecated("This class depends on ModularUI APIs that are scheduled for removal in version 3.2.0. Use `GenericSyncValue.builder()` directly instead. Refer to the `useBuilder` method in this class a migration example.",
+    ReplaceWith("GenericSyncValue.builder(ClayLaser::class.java)"),
+)
+@ApiStatus.ScheduledForRemoval(inVersion = "1.0.0.0")
+class ClayLaserSyncValue(
+    private val getter: Supplier<ClayLaser?>,
+    private val setter: Consumer<ClayLaser?>,
+) : GenericSyncValue<ClayLaser?>(
+    ClayLaser::class.java,
+    getter,
+    setter,
+    ClayLaserByteBufAdapter.INSTANCE,
+    null,
+    true,
+) {
+
+    fun useBuilder(): GenericSyncValue<ClayLaser?> {
+        return GenericSyncValue.builder(ClayLaser::class.java)
+            .getter(this.getter)
+            .setter(this.setter)
+            .adapter(ClayLaserByteBufAdapter.INSTANCE)
+            .build()
+    }
+
+}
